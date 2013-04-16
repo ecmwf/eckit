d0492bd9edabdbccf7b8a878b8248279b474e039
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-04-16 15:11:34 +0100

    move eclib/filesystem/FileHandle.h to eckit/filesystem/FileHandle.h

--- src/eckit/filesystem/FileDescHandle.cc
+++ src/eckit/filesystem/FileDescHandle.cc
@@ -8,7 +8,7 @@
  * does it submit to any jurisdiction.
  */
 
-#include "eclib/filesystem/FileDescHandle.h"
+#include "eckit/filesystem/FileDescHandle.h"
 
 //-----------------------------------------------------------------------------
 

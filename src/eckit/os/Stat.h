91d5fd4983b2afa91d3a16d8312c962c85ff9fda
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-05-13 11:41:51 +0100

    fix missing include guards

--- src/eckit/os/Stat.h
+++ src/eckit/os/Stat.h
@@ -1,3 +1,16 @@
+/*
+ * (C) Copyright 1996-2013 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef eckit_os_Stat_h
+#define eckit_os_Stat_h
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -44,3 +57,5 @@ private:
 //-----------------------------------------------------------------------------
 
 } // namespace eckit
+
+#endif

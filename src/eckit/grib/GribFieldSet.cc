74a1fb66fe6fefa089e360d495f60bfa034276dd
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-11-07 16:50:21 +0000

    work on compute

--- src/eckit/grib/GribFieldSet.cc
+++ src/eckit/grib/GribFieldSet.cc
@@ -76,42 +76,6 @@ void GribFieldSet::write(DataHandle& h) const {
 
 //========================================================================================================
 
-GribFieldSet GribFieldSet::mean()
-{
 
-    GribFieldSet out(1);
-
-    size_t nfields = this->count();
-
-    Log::info() << "mean(" << nfields << ")" << endl;
-
-    ASSERT(nfields) ;
-
-    size_t npoints = 0;
-    GribField* first = this->willAdopt(0);
-    const double* values = first->getValues(npoints);
-    double* result = new double[npoints];
-    ASSERT(result);
-
-    std::copy(values, values + npoints, result);
-
-    for(size_t j = 1; j < nfields; ++j) {
-        size_t n = 0;
-        const GribField* first = this->get(0);
-        const double* values = first->getValues(n);
-        ASSERT(n == npoints);
-        for(size_t i = 0; i < npoints; ++i) {
-            result[i] += values[i];
-        }
-    }
-
-    for(size_t i = 0; i < npoints; ++i) {
-        result[i] /= npoints;
-    }
-
-    out.add(new GribField(first, result, npoints));
-
-    return out;
-}
 
 }

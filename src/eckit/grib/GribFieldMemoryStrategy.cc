a636b7cf3ba3b6694386ac879c061d29359311bf
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-11-18 15:11:53 +0000

    add GribFieldMemoryStrategy.cc

---
+++ src/eckit/grib/GribFieldMemoryStrategy.cc
@@ -0,0 +1,33 @@
+#include "GribFieldMemoryStrategy.h"
+
+namespace eckit {
+
+
+GribFieldMemoryStrategy::GribFieldMemoryStrategy()
+{
+}
+
+GribFieldMemoryStrategy::~GribFieldMemoryStrategy()
+{
+}
+
+void GribFieldMemoryStrategy::touch(GribField&)
+{
+}
+
+void GribFieldMemoryStrategy::release(GribField&)
+{
+}
+
+void GribFieldMemoryStrategy::remove(GribField&)
+{
+}
+
+void GribFieldMemoryStrategy::newValues(GribField&)
+{
+}
+
+void GribFieldMemoryStrategy::newHandle(GribField&)
+{
+}
+}

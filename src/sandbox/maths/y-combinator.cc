acd9cc431585075ad7fb14acd49b8fedfe7e84a1
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-11-24 09:59:51 +0000

    ycombinator!

--- src/sandbox/maths/y-combinator.cc
+++ src/sandbox/maths/y-combinator.cc
@@ -46,60 +46,21 @@ void YCombinator::run()
 {
 
 
-    Math a( 2. );
-    Math b( 4. );
-    Math c( 7. );
-    Math z( 0. );
 
-    if(0)
-    {
-        Math g = a > z;
-        cout << g << endl;
+    Math X = maths::lambda("recurse", "n",
+                           maths::ifelse(Math("n") == Math(0.0), Math(1.0),
+                                         Math(maths::call(Math("recurse"),Math("recurse"), Math("n") - Math(1.0))) * Math("n")));
 
-        cout << g() << endl;
-    }
+    Math Y = maths::lambda("builder", "x",
+                           maths::call(Math("builder"),Math("builder"), Math("x")
+                                       ));
 
-    if(0) {
 
-        Math e = maths::lambda("1", "2",
-                               ifelse(Math("1") > Math("2"), b, c));
-
-        cout << e << endl;
-
-        e = maths::call(e);
-        cout << e(a, z) << endl;
-    }
-
-    if(0)
-    {
-        Math s = Math("a") - Math("b");
-        Math f = maths::lambda("a", "b", s);
-        Math c = Math(maths::call(f))( Math(1.0), Math(3.0));
-        cout << c << endl;
-        cout << c() << endl;
-    }
-
-    if(1)
-    {
-        Math s = Math("n") - Math(1.0);
-        Math r = maths::call(Math("recurse"),Math("recurse"), s);
-        Math e = Math("n") == Math(0.0);
-        Math i = maths::ifelse(e, Math(1.0),  r * Math("n"));
-
-        Math X = maths::lambda("recurse", "n", i);
-
-        Math Y = maths::lambda("builder", "x",
-                               maths::call(Math("builder"),Math("builder"), Math("x")
-                                                                  ));
-        //cout << X << endl;
-        //cout << Y << endl;
-
-        Math Z = maths::call(Y, X, Math(5.0));
-        cout << "-----------------------" << endl;
-        cout << Z << endl;
-        cout << Z() << endl;
-    }
-
+    Math Z = maths::call(Y, X, Math(5.0));
+    cout << "-----------------------" << endl;
+    cout << Z << endl;
+    cout << "-----------------------" << endl;
+    cout << Z() << endl;
 
 }
 

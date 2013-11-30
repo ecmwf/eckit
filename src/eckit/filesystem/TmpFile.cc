e9f9bc785a6e8a976bb28aeee52cd83867250e4b
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-11-30 14:56:55 +0000

    work on tmpfile

--- src/eckit/filesystem/TmpFile.cc
+++ src/eckit/filesystem/TmpFile.cc
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2013 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -24,7 +24,9 @@ static PathName tmp() {
     if(!tmpdir) {
         tmpdir = "/tmp";
     }
-    char path[PATH_MAX];
+
+    long max = pathconf(tmpdir, _PC_PATH_MAX);
+    char path[max];
     sprintf(path, "%s/eckitXXXXXXXXXXX", tmpdir);
     int fd;
     SYSCALL(fd = ::mkstemp(path));
@@ -46,7 +48,7 @@ TmpFile::TmpFile():
 
 TmpFile::~TmpFile()
 {
-	unlink();
+    unlink();
 }
 
 //-----------------------------------------------------------------------------

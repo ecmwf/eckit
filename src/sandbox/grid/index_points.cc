743ffd30a4a295c50b8639d9dbf447d9f3b90c14
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-03-19 00:00:18 +0000

    filter duplicate points and returns index

--- src/sandbox/grid/index_points.cc
+++ src/sandbox/grid/index_points.cc
@@ -47,18 +47,72 @@ using namespace eckit;
 
 //------------------------------------------------------------------------------------------------------
 
-#define NLATS 19
-#define NLONG 19
+#define NLATS 4
+#define NLONG 4
 
 //------------------------------------------------------------------------------------------------------
 
-class UniquePoint {
+class PointSet {
 
     typedef std::map< size_t, size_t > DupStore_t;
 
 public:
 
-    UniquePoint( PointIndex3& tree ) : tree_(tree) { reset(); }
+    PointSet( std::vector< Point3 >* ipts )
+    {
+        reset();
+
+        // std::cout << "building kdtree ... " << std::endl;
+
+        npts_ = ipts->size();
+
+        std::vector< PointIndex3::Value > pidx;
+        pidx.reserve(npts_);
+
+        for( size_t ip = 0; ip < npts_; ++ip )
+        {
+            Point3& p = (*ipts)[ip]; /* std::cout << p << std::endl; */
+            pidx.push_back( PointIndex3::Value( PointIndex3::Point(p), ip ) );
+        }
+
+        PathName path (std::string("~/tmp/cache/grid/") + "test" + ".kdtree");
+        PathName tmp  (std::string("~/tmp/cache/grid/") + "test" + ".tmp");
+        tmp.unlink();
+
+        tree_ = new PointIndex3(tmp, npts_, 0);
+
+        tree_->build(pidx.begin(), pidx.end());
+
+        PathName::rename(tmp, path);
+    }
+
+    ~PointSet(){ delete tree_; }
+
+    std::vector< KPoint3 >* list_unique_points()
+    {
+        std::vector< KPoint3 >* opts = new std::vector< KPoint3 >( );
+        opts->reserve(npts_);
+
+        // std::cout << "computing duplicates ... " << std::endl;
+
+        for( PointIndex3::iterator i = tree_->begin(); i != tree_->end(); ++i )
+        {
+            KPoint3 p (  i->point() );
+            size_t  ip = i->payload();
+            std::cout << "point " << ip << " " << p << std::endl;
+            size_t uidx = unique(p,ip);
+            if( ip == uidx )
+            {
+                std::cout << "----> UNIQ " << ip << std::endl;
+                opts->push_back(p);
+            }
+            else
+            {
+                std::cout << "----> DUP " << ip << " -> " << uidx << std::endl;
+            }
+        }
+        return opts;
+    }
 
     size_t unique( eckit::KPoint3& p, size_t idx )
     {
@@ -74,7 +128,7 @@ public:
 
 //        DBGX(Kn());
 
-        PointIndex3::NodeList nearest = tree_.kNearestNeighbours(p,Kn());
+        PointIndex3::NodeList nearest = tree_->kNearestNeighbours(p,Kn());
 
         std::vector<size_t> equals;
         equals.reserve( nearest.size() );
@@ -135,7 +189,8 @@ protected:
 
 private:
     size_t n_;
-    PointIndex3& tree_;
+    size_t npts_;
+    PointIndex3* tree_;
     DupStore_t duplicates_; ///< map from duplicate idx to idx representing group of points
 };
 
@@ -149,64 +204,27 @@ int main()
 
 //    std::vector< Point3 >* ipts = atlas::MeshGen::generate_latlon_points(NLATS, NLONG);
     std::vector< Point3 >* ipts = atlas::MeshGen::generate_latlon_grid(NLATS, NLONG);
-    const size_t npts = ipts->size();
 
-    std::cout << "initial points " << npts << std::endl;
+    std::cout << "initial points " << ipts->size() << std::endl;
 
-    std::vector< KPoint3 > opts;
-    opts.reserve(npts);
+    PointSet points( ipts );
 
-    /// BUILD KD-TREE ////////////////////////////////////////////////////////////
+    std::vector< KPoint3 >* opts = points.list_unique_points();
 
-    std::cout << "building kdtree ... " << std::endl;
+    std::cout << "duplicates  points " << points.duplicates().size() << std::endl;
+    std::cout << "unique  points " << opts->size() << std::endl;
 
-    std::vector< PointIndex3::Value > pidx;
-    pidx.reserve(npts);
+    // print indexes of unique points
 
-    for( size_t ip = 0; ip < npts; ++ip )
+    for( size_t i = 0; i < opts->size(); ++i )
     {
-        Point3& p = (*ipts)[ip];
-
-        std::cout << p << std::endl;
-
-        pidx.push_back( PointIndex3::Value( PointIndex3::Point(p), ip ) );
+        std::cout << points.unique( (*opts)[i], std::numeric_limits<size_t>::max() ) << std::endl;
     }
 
-    PathName path (std::string("~/tmp/cache/grid/") + "test" + ".kdtree");
-    PathName tmp  (std::string("~/tmp/cache/grid/") + "test" + ".tmp");
-    tmp.unlink();
-
-    PointIndex3* tree = new PointIndex3(tmp, npts, 0);
-
-    tree->build(pidx.begin(), pidx.end());
-
-    PathName::rename(tmp, path);
-
-    /// SEARCH ////////////////////////////////////////////////////////////
-
-    UniquePoint uniq( *tree );
-
-    for( size_t ip = 0; ip < ipts->size(); ++ip )
-    {
-        KPoint3 p (  (*ipts)[ip] );  /* std::cout << "point " << ip << " " << p << std::endl; */
-
-        size_t uidx = uniq.unique(p,ip);
-        if( ip == uidx )
-        {
-//            std::cout << "----> UNIQ " << ip << std::endl;
-            opts.push_back(p);
-        }
-        else
-        {
-//            std::cout << "----> DUP " << ip << " -> " << uidx << std::endl;
-        }
-    }
-
-    std::cout << "duplicates  points " << uniq.duplicates().size() << std::endl;
-    std::cout << "unique  points " << opts.size() << std::endl;
+    // free memory
 
     delete ipts;
-    delete tree;
+    delete opts;
 
     return 0;
 }

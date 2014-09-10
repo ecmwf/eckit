5eb4035a2e16c4c535a7f786a12368afc868865a
Tiago Quintino <tiago.quintino@ecmwf.int>
2014-09-09 20:45:41 -0700

    Implemented a DenseMap that uses internal dense storage

---
+++ src/tests/container/test_densemap.cc
@@ -0,0 +1,115 @@
+/*
+ * (C) Copyright 1996-2012 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include <cmath>
+
+// #define BOOST_TEST_MODULE test_eckit_container
+// #include "ecbuild/boost_test_framework.h"
+
+#include "eckit/exception/Exceptions.h"
+#include "eckit/container/DenseMap.h"
+
+using namespace std;
+using namespace eckit;
+
+//-----------------------------------------------------------------------------
+
+// BOOST_AUTO_TEST_SUITE( test_eckit_container_densemap )
+
+// BOOST_AUTO_TEST_CASE( test_eckit_container_densemap_0 )
+// {
+
+//   DenseMap<std::string,int> m;
+
+//   BOOST_CHECK_NO_THROW( m.insert( "two",  2 ) );
+//   BOOST_CHECK_NO_THROW( m.insert( "four", 4 ) );
+//   BOOST_CHECK_NO_THROW( m.insert( "nine", 9 ) );
+
+//   BOOST_CHECK_EQUAL( m.size(), 3 );
+
+// }
+
+// BOOST_AUTO_TEST_SUITE_END()
+
+int main()
+{
+    DenseMap<std::string,int> m;
+
+    //
+
+    m.insert( "two",  2 );
+
+    ASSERT( !m.sorted() );
+
+    ASSERT( m.size() == 1 );
+
+    m.sort();
+
+    ASSERT( m.sorted() );
+
+    ASSERT( m.get("two") == 2 );
+
+    //
+
+    m.insert( "four", 4 );
+    m.insert( "nine", 9 );
+
+    ASSERT( !m.sorted() );
+
+    m.sort();
+
+    ASSERT( m.find("four")->second == 4 );
+    ASSERT( m.find("nine")->second == 9 );
+
+    ASSERT( m.sorted() );
+
+    ASSERT( m.size() == 3 );
+
+    ASSERT( m.get("two") == 2 );
+    ASSERT( m.get("four") == 4 );
+    ASSERT( m.get("nine") == 9 );
+
+    // failed find
+
+    ASSERT( m.find("one") == m.end() );
+
+    // replace an existing value
+
+    m.replace( "four", 44 );  
+    m.replace( "nine", 99 );
+
+    ASSERT( m.sorted() ); // still sorted
+
+    ASSERT( m.size() == 3 );
+
+    ASSERT( m.get("two") == 2 );
+    ASSERT( m.get("four") == 44 );
+    ASSERT( m.get("nine") == 99 );
+
+    // replace into non-existing
+
+    ASSERT( m.size() == 3 );
+
+    m.replace( "five", 555 );  
+
+    ASSERT( !m.sorted() );
+    m.sort();
+
+    ASSERT( m.get("two") == 2 );
+    ASSERT( m.get("four") == 44 );
+    ASSERT( m.get("nine") == 99 );
+    ASSERT( m.get("five") == 555 );
+
+    ASSERT( m.size() == 4 );
+
+    //
+    // std::cout << m << std::endl;
+
+}

675eb4ac3ea0272af6d92e777fdd87f5aacb16fb
Willem Deconinck <willem.deconinck@ecmwf.int>
2014-09-23 16:59:11 +0100

    MultiChannel query functions

--- src/eckit/log/MultiChannel.cc
+++ src/eckit/log/MultiChannel.cc
@@ -1,13 +1,14 @@
 /*
  * (C) Copyright 1996-2013 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
+#include "eckit/exception/Exceptions.h"
 #include "eckit/log/Log.h"
 #include "eckit/log/MultiChannel.h"
 
@@ -21,25 +22,25 @@ class MultiplexBuffer: public std::streambuf {
 public:
 
     typedef std::map<std::string,ostream_handle> streams_t;
-    
+
     MultiplexBuffer( std::size_t size = 1024 ) :
         std::streambuf(),
-        buffer_( size + 1 ) // + 1 so we can always write the '\0'        
+        buffer_( size + 1 ) // + 1 so we can always write the '\0'
     {
-        assert( size );        
+        assert( size );
         char *base = &buffer_.front();
         setp( base, base + buffer_.size() - 1 ); // don't consider the space for '\0'
         setg(0, 0, 0);
     }
-    
+
     ~MultiplexBuffer() { sync(); }
-    
+
     streams_t streams_;
-    
+
 private:
 
   std::vector<char> buffer_;
-  
+
   bool dumpBuffer()
   {
       for( streams_t::iterator i = streams_.begin(); i != streams_.end(); ++i )
@@ -50,9 +51,9 @@ private:
       setp(pbase(), epptr());
       return true;
   }
-  
+
 protected:
-  
+
   virtual int_type overflow(int_type ch)
   {
       if (ch == traits_type::eof() ) { return sync(); }
@@ -60,7 +61,7 @@ protected:
       sputc(ch);
       return traits_type::to_int_type(ch);
   }
-  
+
   virtual int_type sync()
   {
       if( dumpBuffer() )
@@ -71,12 +72,12 @@ protected:
       }
       else return -1;
   }
-  
+
 };
 
 //-----------------------------------------------------------------------------
 
-MultiChannel::MultiChannel() : 
+MultiChannel::MultiChannel() :
     Channel( new MultiplexBuffer() ),
     buff_( dynamic_cast<MultiplexBuffer*>( rdbuf() ) )
 {
@@ -88,7 +89,7 @@ MultiChannel::~MultiChannel()
 }
 
 bool MultiChannel::remove(const std::string &k)
-{ 
+{
     MultiplexBuffer::streams_t& streams = buff_->streams_;
     MultiplexBuffer::streams_t::iterator i = streams.find(k);
     if( i != streams.end() )
@@ -111,6 +112,28 @@ void MultiChannel::add(const std::string &k, std::ostream &s)
 {
     remove(k);
     buff_->streams_[k].reset(s);
+}
+
+bool MultiChannel::has(const std::string &k) const
+{
+  MultiplexBuffer::streams_t& streams = buff_->streams_;
+  MultiplexBuffer::streams_t::iterator i = streams.find(k);
+  if( i == streams.end() )
+  {
+    return false;
+  }
+  return true;
+}
+
+std::ostream& MultiChannel::get(const std::string &k)
+{
+  MultiplexBuffer::streams_t& streams = buff_->streams_;
+  MultiplexBuffer::streams_t::iterator i = streams.find(k);
+  if( i == streams.end() )
+  {
+    throw OutOfRange("Could not find stream "+k+" in MultiChannel",Here());
+  }
+  return *i->second.get();
 }
 
 void MultiChannel::clear()

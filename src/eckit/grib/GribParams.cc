fdee93c37ab1fdb5f4f51d3318e6f56e00489e13
Avi Bahra <Avi.Bahra@ecmwf.int>
2014-12-04 16:44:33 +0000

    ECKIT-24

--- src/eckit/grib/GribParams.cc
+++ src/eckit/grib/GribParams.cc
@@ -15,6 +15,7 @@
 
 #include "eckit/grib/GribParams.h"
 #include "eckit/grib/GribAccessor.h"
+#include "eckit/grib/GribMutator.h"
 
 namespace eckit {
 namespace grib {
@@ -54,36 +55,86 @@ GribParams::GribParams(GribHandle& gh)
 
 	set("GRIB.geographyHash", the_hash);
 
+	// *****************************************************************************
+	// get the scanning mode, needed for bounding box
+	// and to align iterators to atlas/mir defaults
+	// make sure any access to grib iterators, returns the points according to our defaults: i.e
+	//    iScansPositively(true),
+	//    jScansPositively(false),
+	//    jPointsAreConsecutive(false)
+	//    alternativeRowScanning(false)
+	//**NOTE: GribParams does not currently iterate over the points, since they are generated.
+	//
+	bool iScansPositively = true;
+	if (gh.hasKey("iScansPositively")) {
+
+	   iScansPositively = GribAccessor<bool>("iScansPositively")(gh);
+	   if ( !iScansPositively ) {
+	      GribMutator<bool> mt("iScansPositively");
+	      mt.set(gh,true);
+	   }
+	}
+	bool jScansPositively = false;
+   if (gh.hasKey("jScansPositively")) {
+
+	   jScansPositively = GribAccessor<bool>("jScansPositively")(gh);
+	   if (jScansPositively) {
+         GribMutator<bool> mt("jScansPositively");
+         mt.set(gh,false);
+	   }
+	}
+   if (gh.hasKey("jPointsAreConsecutive")) {
+      bool consec = GribAccessor<bool>("jPointsAreConsecutive")(gh);
+      if (consec) {
+         GribMutator<bool> mt("jPointsAreConsecutive");
+         mt.set(gh,false);
+      }
+   }
+   if (gh.hasKey("alternativeRowScanning")) {
+      // Available in GRIB, but not supported, assert if we come across it.
+      bool alter = GribAccessor<bool>("alternativeRowScanning")(gh);
+      ASSERT(!alter);
+   }
+   // *****************************************************************************
+
+
 	// Not all GRID's have a bounding box, i.e Polar Stereographic
+   // GRIB1 [ -180, +180 ] || [ 0, 360 ]
+   // GRIB2 [ 0, 360 ]
 	if (gh.hasKey("latitudeOfLastGridPointInDegrees"))
 	{
-	   double north = gh.latitudeOfFirstGridPointInDegrees();
-	   double south = gh.latitudeOfLastGridPointInDegrees();
-	   double west  = gh.longitudeOfFirstGridPointInDegrees();
-	   double east  = gh.longitudeOfLastGridPointInDegrees();
+	   double lat_1 = gh.latitudeOfFirstGridPointInDegrees();
+	   double lat_2 = gh.latitudeOfLastGridPointInDegrees();
+	   double lon_1  = gh.longitudeOfFirstGridPointInDegrees();
+	   double lon_2  = gh.longitudeOfLastGridPointInDegrees();
 
-	   eckit::geometry::reduceTo2Pi(west);
-	   eckit::geometry::reduceTo2Pi(east);
+	   if (iScansPositively) ASSERT(lon_2 > lon_1);
+	   else                  ASSERT(lon_2 < lon_1);
 
-	   // ignore scanning mode:
-	   north_ = std::max(north,south);
-	   south_ = std::min(north,south);
-	   east_ = std::max(east,west);
-	   west_ = std::min(east,west);
+	   west_ = std::min(lon_1,lon_2);
+	   east_ = std::max(lon_2,lon_1);
 
-	   set("bbox_n", north_ );
-	   set("bbox_s", south_ );
-	   set("bbox_w", west_  );
-	   set("bbox_e", east_  );
+	   eckit::geometry::reduceTo2Pi(lon_1);
+	   eckit::geometry::reduceTo2Pi(lon_2);
+
+
+      if (jScansPositively) ASSERT( lat_1 < lat_2 );
+      else                  ASSERT( lat_1 > lat_2 );
+
+	   // For north south, we can ignore scanning mode:
+	   north_ = std::max(lat_1,lat_2);
+	   south_ = std::min(lat_1,lat_2);
 
 	   // check area
-	   ASSERT(north_ > south_); // This assertion only make sense if we ignore scanning mode
 	   ASSERT(north_ < 90.0  || FloatCompare::is_equal(north_,90.0,degreesEps_));
 	   ASSERT(south_ < 90.0  || FloatCompare::is_equal(south_,90.0,degreesEps_));
 	   ASSERT(north_ > -90.0 || FloatCompare::is_equal(north_,-90.0,degreesEps_));
 	   ASSERT(south_ > -90.0 || FloatCompare::is_equal(south_,-90.0,degreesEps_));
 
-	   ASSERT(east_ > west_); // This assertion only make sense if we ignore scanning mode
+      set("bbox_n", north_ );
+      set("bbox_s", south_ );
+      set("bbox_w", west_  );
+      set("bbox_e", east_  );
 	}
 
 	no_of_data_points_ = gh.npts();

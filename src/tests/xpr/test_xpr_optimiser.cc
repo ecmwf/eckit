540efff83f25258613c5d32876a516ba907091c1
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-02-02 11:15:41 +0000

    ECKIT-23 Properly test Expression code representation

--- src/tests/xpr/test_xpr_optimiser.cc
+++ src/tests/xpr/test_xpr_optimiser.cc
@@ -63,7 +63,7 @@ BOOST_AUTO_TEST_CASE( test_add_vv )
     Xpr X = xpr::add(x,y);
 
     BOOST_CHECK( X.optimise().expr()->str() == "Add(Vector(5, 5, 5), Vector(7, 7, 7))" );
-    BOOST_CHECK( X.optimise().expr()->code() == "(xpr::vector(5, 5, 5) + xpr::vector(7, 7, 7))" );
+    BOOST_CHECK( X.optimise().expr()->code() == "xpr::add(xpr::vector({5, 5, 5}), xpr::vector({7, 7, 7}))" );
     BOOST_CHECK( X.optimise().expr()->json() == "{\"xpr::add\":[[5,5,5],[7,7,7]]}" );
 }
 
@@ -72,7 +72,7 @@ BOOST_AUTO_TEST_CASE( test_list_add_ss_vv )
     Xpr X = xpr::list( xpr::add(a,b), xpr::add(x,y));
 
     BOOST_CHECK( X.optimise().expr()->str() == "List(Scalar(6), Add(Vector(5, 5, 5), Vector(7, 7, 7)))" );
-    BOOST_CHECK( X.optimise().expr()->code() == "xpr::list(xpr::scalar(6), (xpr::vector(5, 5, 5) + xpr::vector(7, 7, 7)))" );
+    BOOST_CHECK( X.optimise().expr()->code() == "xpr::list(xpr::scalar(6), xpr::add(xpr::vector({5, 5, 5}), xpr::vector({7, 7, 7})))" );
     BOOST_CHECK( X.optimise().expr()->json() == "{\"xpr::list\":[6,{\"xpr::add\":[[5,5,5],[7,7,7]]}]}" );
 }
 
@@ -83,7 +83,7 @@ BOOST_AUTO_TEST_CASE( test_linear_sv_sv )
     Xpr X = xpr::add( xpr::prod(e,x), xpr::prod(z,y));
 
     BOOST_CHECK( X.optimise().expr()->str() == "Linear(Scalar(6), Vector(5, 5, 5), Scalar(-12), Vector(7, 7, 7))" );
-    BOOST_CHECK( X.optimise().expr()->code() == "xpr::linear(xpr::scalar(6), xpr::vector(5, 5, 5), xpr::scalar(-12), xpr::vector(7, 7, 7)))" );
+    BOOST_CHECK( X.optimise().expr()->code() == "xpr::linear(xpr::scalar(6), xpr::vector({5, 5, 5}), xpr::scalar(-12), xpr::vector({7, 7, 7})))" );
     BOOST_CHECK( X.optimise().expr()->json() == "{\"xpr::linear\":[6,[5,5,5],-12,[7,7,7]]}" );
 }
 
@@ -97,7 +97,7 @@ BOOST_AUTO_TEST_CASE( test_linear_with_count )
     Xpr X = xpr::add( xpr::prod(e,x), xpr::prod(b,y));
 
     BOOST_CHECK( X.optimise().expr()->str() == "Linear(Scalar(10), Vector(5, 5, 5), Scalar(4), Vector(7, 7, 7))" );
-    BOOST_CHECK( X.optimise().expr()->code() == "xpr::linear(xpr::scalar(10), xpr::vector(5, 5, 5), xpr::scalar(4), xpr::vector(7, 7, 7)))" );
+    BOOST_CHECK( X.optimise().expr()->code() == "xpr::linear(xpr::scalar(10), xpr::vector({5, 5, 5}), xpr::scalar(4), xpr::vector({7, 7, 7})))" );
     BOOST_CHECK( X.optimise().expr()->json() == "{\"xpr::linear\":[10,[5,5,5],4,[7,7,7]]}" );
 }
 

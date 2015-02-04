2a650c02ce685dbd54cf55730e9f4b48d7938456
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-02-04 18:31:25 +0000

    ECKIT-37 Make Properties serializable

--- src/eckit/value/Properties.cc
+++ src/eckit/value/Properties.cc
@@ -19,6 +19,18 @@ Properties::Properties()
 {
 }
 
+Properties::Properties(Stream &s)
+{
+    PropertyMap::size_type len;
+    s >> len;
+    for (int i = 0; i < len; ++i) {
+        std::string k;
+        s >> k;
+        property_t v(s);
+        props_[k] = v;
+    }
+}
+
 bool Properties::has(const std::string &k) const
 {
     return ( props_.find(k) != props_.end() );
@@ -61,6 +73,7 @@ void Properties::print( std::ostream& s ) const
 
 void Properties::encode( Stream& s ) const
 {
+    s << props_.size();
     for( PropertyMap::const_iterator vit = props_.begin(); vit != props_.end(); ++vit )
         s << (*vit).first << (*vit).second;
 }

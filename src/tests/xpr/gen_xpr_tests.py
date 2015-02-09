c46033d3437455e21e3a85db7f97500cbda1dae6
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-02-09 10:45:59 +0000

    ECKIT-36 test_xpr_operators: test boolean operators

--- src/tests/xpr/gen_xpr_tests.py
+++ src/tests/xpr/gen_xpr_tests.py
@@ -33,33 +33,48 @@ BOOST_AUTO_TEST_CASE( test_{op_c}_{a_c}_{b_c} )
 }}
 """
 
+
+def run_operator_test(op, sym, a, a_t, b, b_t, res_t):
+    res = eval("%s %s %s" % (a, sym, b))
+    if res_t == 'boolean':
+        res = 'true' if res else 'false'
+    # Real expressions are represented as int if they're not decimal
+    elif res == int(res):
+        res = int(res)
+    return xpr_operator_tpl.format(a=a,
+                                   ad=int(a),
+                                   a_c=camelcase(a_t),
+                                   a_f=f(a_t),
+                                   b=b,
+                                   bd=int(b),
+                                   b_c=camelcase(b_t),
+                                   b_f=f(b_t),
+                                   op_c=camelcase(op),
+                                   op_f=f(op),
+                                   res=res,
+                                   res_c=camelcase(res_t),
+                                   res_f=f(res_t),
+                                   sym=sym)
+
+
 def gen_operator_tests(testfile):
     tests = []
-    arithmetic_ops = [('add', '+'), ('sub', '-'), ('div', '/'), ('prod', '*')]
-    arithmetic_args = [(2, 'integer', 4., 'real', 'real'),
-                       (2., 'real', 4, 'integer', 'real'),
-                       (2., 'real', 4., 'real', 'real'),
-                       (2, 'integer', 4, 'integer', 'integer')]
-    for op, sym in arithmetic_ops:
-        for a, a_t, b, b_t, res_t in arithmetic_args:
-            res = eval("%s %s %s" % (a, sym, b))
-            # Real expressions are represented as int if they're not decimal
-            if res == int(res):
-                res = int(res)
-            tests.append(xpr_operator_tpl.format(a=a,
-                                                 ad=int(a),
-                                                 a_c=camelcase(a_t),
-                                                 a_f=f(a_t),
-                                                 b=b,
-                                                 bd=int(b),
-                                                 b_c=camelcase(b_t),
-                                                 b_f=f(b_t),
-                                                 op_c=camelcase(op),
-                                                 op_f=f(op),
-                                                 res=res,
-                                                 res_c=camelcase(res_t),
-                                                 res_f=f(res_t),
-                                                 sym=sym))
+    # Arithmetic operators
+    for op, sym in [('add', '+'), ('sub', '-'), ('div', '/'), ('prod', '*')]:
+        for a, a_t, b, b_t, res_t in [(2, 'integer', 4., 'real', 'real'),
+                                      (2., 'real', 4, 'integer', 'real'),
+                                      (2., 'real', 4., 'real', 'real'),
+                                      (2, 'integer', 4, 'integer', 'integer')]:
+            tests.append(run_operator_test(op, sym, a, a_t, b, b_t, res_t))
+    # Boolean operators
+    for op, sym in [('equal', '=='), ('not_equal', '!='),
+                    ('greater', '>'), ('less', '<'),
+                    ('greater_equal', '>='), ('less_equal', '<=')]:
+        for a, a_t, b, b_t in [(2, 'integer', 4., 'real'),
+                               (2., 'real', 4, 'integer'),
+                               (2., 'real', 4., 'real'),
+                               (2, 'integer', 4, 'integer')]:
+            tests.append(run_operator_test(op, sym, a, a_t, b, b_t, 'boolean'))
     return testfile % {'test_cases': '\n\n'.join(tests)}
 
 if __name__ == '__main__':

879780625438e53704c05e923775636715da5182
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-03-20 16:18:02 +0000

    ECKIT-46 Simplify ForEach unit test

--- src/tests/xpr/test_xpr_foreach.cc
+++ src/tests/xpr/test_xpr_foreach.cc
@@ -35,23 +35,17 @@ BOOST_AUTO_TEST_SUITE( test_eckit_xpr_foreach )
 
 BOOST_AUTO_TEST_CASE( test_lambda_list )
 {
-    Xpr a = xpr::real( 2. );
-    Xpr b = xpr::real( 4. );
-    Xpr c = xpr::real( 1. );
-
-    Xpr l = xpr::list( a, b, c );
-
+    Xpr l = xpr::list( xpr::real( 2. ), xpr::real( 4. ), xpr::real( 1. ) );
     Xpr trice = call(lambda("a", Xpr(3.0) * Xpr("a")));
+    Xpr loop = xpr::forEach( l, trice );
 
-    ExpPtr loop = xpr::forEach( l, trice );
+    BOOST_CHECK_EQUAL( loop.str(), "ForEach(List(Real(2), Real(4), Real(1)), Call(Lambda(=(a), Prod(Real(3), _(a)))))" );
+    BOOST_CHECK_EQUAL( loop.code(), "xpr::forEach(xpr::list(xpr::real(2), xpr::real(4), xpr::real(1)), xpr::call(xpr::lambda(\"a\", xpr::prod(xpr::real(3), xpr::parameter(\"a\")))))" );
+    BOOST_CHECK_EQUAL( loop.json(), "{\"xpr::forEach\":[{\"xpr::list\":[2,4,1]},{\"xpr::call\":[{\"xpr::lambda\":[{\"xpr::paramdef\":[]},{\"xpr::prod\":[3,{\"xpr::parameter\":[]}]}]}]}]}" );
 
-    BOOST_CHECK_EQUAL( loop->str(), "ForEach(List(Real(2), Real(4), Real(1)), Call(Lambda(=(a), Prod(Real(3), _(a)))))" );
-    BOOST_CHECK_EQUAL( loop->code(), "xpr::forEach(xpr::list(xpr::real(2), xpr::real(4), xpr::real(1)), xpr::call(xpr::lambda(\"a\", xpr::prod(xpr::real(3), xpr::parameter(\"a\")))))" );
-    BOOST_CHECK_EQUAL( loop->json(), "{\"xpr::forEach\":[{\"xpr::list\":[2,4,1]},{\"xpr::call\":[{\"xpr::lambda\":[{\"xpr::paramdef\":[]},{\"xpr::prod\":[3,{\"xpr::parameter\":[]}]}]}]}]}" );
-
-    BOOST_CHECK_EQUAL( loop->eval()->str(), "List(Real(6), Real(12), Real(3))" );
-    BOOST_CHECK_EQUAL( loop->eval()->code(), "xpr::list(xpr::real(6), xpr::real(12), xpr::real(3))" );
-    BOOST_CHECK_EQUAL( loop->eval()->json(), "{\"xpr::list\":[6,12,3]}" );
+    BOOST_CHECK_EQUAL( loop.eval().str(), "List(Real(6), Real(12), Real(3))" );
+    BOOST_CHECK_EQUAL( loop.eval().code(), "xpr::list(xpr::real(6), xpr::real(12), xpr::real(3))" );
+    BOOST_CHECK_EQUAL( loop.eval().json(), "{\"xpr::list\":[6,12,3]}" );
 }
 
 //-----------------------------------------------------------------------------

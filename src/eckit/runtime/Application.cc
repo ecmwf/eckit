73013c9ac836b1ad3926deda533df5b932ca110c
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-05 15:53:54 +0100

    ECKIT-55 Refactor Application: remove template arguments
    Define policies through constructor arguments with defaults.

--- src/eckit/runtime/Application.cc
+++ src/eckit/runtime/Application.cc
@@ -1,11 +1,11 @@
 /*
- * (C) Copyright 1996-2013 ECMWF.
- * 
+ * (C) Copyright 1996-2015 ECMWF.
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
- * granted to it by virtue of its status as an intergovernmental organisation nor
- * does it submit to any jurisdiction.
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
  */
 
 #include <stdlib.h>
@@ -16,10 +16,8 @@
 #include "eckit/log/Log.h"
 #include "eckit/os/Semaphore.h"
 #include "eckit/runtime/Application.h"
+#include "eckit/runtime/Context.h"
 #include "eckit/runtime/Monitor.h"
-#include "eckit/runtime/StandardBehavior.h"
-
-#include "eckit/os/BackTrace.h"
 
 //-----------------------------------------------------------------------------
 
@@ -27,69 +25,246 @@ namespace eckit {
 
 //-----------------------------------------------------------------------------
 
-template<> Application<>* Application<>::instance_ = 0;
-
-//-----------------------------------------------------------------------------
-
 static char* memoryReserve;
 
-static void end(const char* msg)
-{
+static void end(const char* msg) {
     static bool in_end = false;
 
-    if(in_end) {
-        fprintf(stderr,"terminate called twice\n");
+    if (in_end) {
+        fprintf(stderr, "terminate called twice\n");
         _exit(1);
     }
 
     in_end = true;
 
-	delete[] memoryReserve; memoryReserve = 0;
-	Log::monitor(Log::App,9998) << msg << std::endl;
+    delete[] memoryReserve;
+    memoryReserve = 0;
+    Log::monitor(Log::App, 9998) << msg << std::endl;
 
-	try {
-		throw;
-	}
-	catch(std::exception& e)
-	{
-        Log::panic() << "!!!!!!!!!!!!!!!!!! " ;
-		Log::panic() << msg <<  " with the exception:" << std::endl;
-		Log::panic() << e.what() << std::endl;
-	//	Exception::exceptionStack(Log::panic());
-	}
+    try {
+        throw;
+    } catch (std::exception& e) {
+        Log::panic() << "!!!!!!!!!!!!!!!!!! ";
+        Log::panic() << msg << " with the exception:" << std::endl;
+        Log::panic() << e.what() << std::endl;
+        //	Exception::exceptionStack(Log::panic());
+    }
     _exit(1);
-	//Panic(msg);
+    // Panic(msg);
 }
 
 //-----------------------------------------------------------------------------
 
-StdSignalRegist::StdSignalRegist() : regist_(false)
-{
-    memoryReserve = new char[20*1024];    
+void DoMonitor::start() {
+    try  // activate monitor
+    {
+        Monitor::active(true);
+        Monitor::instance().startup();
+        Context::instance().self(Monitor::instance().self());
+    } catch (std::exception& e) {
+        Log::error() << "** " << e.what() << " Caught in " << Here() << std::endl;
+        Log::error() << "** Exception is re-thrown" << std::endl;
+        throw;
+    }
 }
 
-StdSignalRegist::~StdSignalRegist()
-{
-    if( !memoryReserve ) { delete[] memoryReserve;  memoryReserve = 0; }
-}
-
-void StdSignalRegist::catch_terminate()
-{
-	end("Terminate was called");
-}
-
-void StdSignalRegist::catch_unexpected()
-{
-	end("Unexpected was called");
-}
-
-void StdSignalRegist::catch_new_handler()
-{
-	delete[] memoryReserve; memoryReserve = 0;
-	throw OutOfMemory();
+void DoMonitor::stop() {
+    Monitor::instance().shutdown();
 }
 
 //-----------------------------------------------------------------------------
 
-} // namespace eckit
+StdSignalRegist::StdSignalRegist() : regist_(false) {
+    memoryReserve = new char[20 * 1024];
+}
 
+StdSignalRegist::~StdSignalRegist() {
+    if (!memoryReserve) {
+        delete[] memoryReserve;
+        memoryReserve = 0;
+    }
+}
+
+void StdSignalRegist::catch_terminate() {
+    end("Terminate was called");
+}
+
+void StdSignalRegist::catch_unexpected() {
+    end("Unexpected was called");
+}
+
+void StdSignalRegist::catch_new_handler() {
+    delete[] memoryReserve;
+    memoryReserve = 0;
+    throw OutOfMemory();
+}
+
+void StdSignalRegist::regist() {
+    if (regist_) return;
+    nh = std::set_new_handler(StdSignalRegist::catch_new_handler);
+    th = std::set_terminate(StdSignalRegist::catch_terminate);
+    uh = std::set_unexpected(StdSignalRegist::catch_unexpected);
+    regist_ = true;
+}
+
+void StdSignalRegist::unregist() {
+    if (!regist_) return;
+    if (nh) std::set_new_handler(nh);
+    if (th) std::set_terminate(th);
+    if (uh) std::set_unexpected(uh);
+    regist_ = false;
+}
+
+//=============================================================================
+
+Application::Application(int argc, char** argv,
+                         LoggingPolicy* logPolicy,
+                         MonitoringPolicy* monPolicy,
+                         LocationPolicy* locPolicy,
+                         SignallingPolicy* sigPolicy)
+    : running_(false),
+      loggingPolicy_(logPolicy),
+      monitoringPolicy_(monPolicy),
+      locationPolicy_(locPolicy),
+      signallingPolicy_(sigPolicy) {
+    locationPolicy_->setup();  // location policy first -- sets home()
+
+    name_ = PathName(argv[0]).baseName(false);
+
+    Context& ctxt = Context::instance();
+    ctxt.setup(argc, argv);
+    ctxt.runName(name_);
+    ctxt.debug(Resource<int>(this, "debug;$DEBUG;-debug", 0));
+
+    loggingPolicy_->setup();
+
+    monitoringPolicy_->start();
+
+    ::srand(::getpid() + ::time(0));
+
+    // ensure is uniqe instance of this application
+    {
+        if (instance_) throw SeriousBug("An instance of application already exists");
+        instance_ = this;
+    }
+
+    signallingPolicy_->regist();
+
+    Loader::callAll(&Loader::execute);
+}
+
+//-----------------------------------------------------------------------------
+
+Application::~Application() {
+    signallingPolicy_->unregist();
+    monitoringPolicy_->stop();
+    instance_ = 0;
+}
+
+//-----------------------------------------------------------------------------
+
+Application& Application::instance() {
+    PANIC(instance_ == 0);
+    return *instance_;
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::reconfigure() {
+    Log::info() << "Application::reconfigure" << std::endl;
+
+    int debug = Resource<int>(this, "debug;$DEBUG;-debug", 0);
+
+    Context::instance().debug(debug);
+
+    Context::instance().reconfigure();  // forward to context
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::start() {
+    int status = 0;
+
+    std::string displayName = Resource<std::string>("-name", name_);
+
+    Context::instance().displayName(displayName);
+
+    Monitor::instance().name(displayName);
+
+    Log::info() << "** Start of " << name() << " ** pid is " << getpid() << std::endl;
+
+    try {
+        Log::status() << "Running" << std::endl;
+        running_ = true;
+        run();
+        running_ = false;
+    } catch (std::exception& e) {
+        status = 1;
+        Log::error() << "** " << e.what() << " Caught in " << Here() << std::endl;
+        Log::error() << "** Exception is terminates " << name() << std::endl;
+    }
+
+    Log::info() << "** End of " << name() << " (" << Context::instance().argv(0) << ")  **" << std::endl;
+
+    ::exit(status);
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::stop() {
+    ::exit(0);  // or: throw Stop();
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::kill() {
+    ::exit(1);
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::terminate() {
+    stop();
+}
+
+//-----------------------------------------------------------------------------
+
+void Application::unique() {
+    PathName lockFile("~/locks/" + name());
+
+    if (!lockFile.exists()) lockFile.touch();
+
+    Semaphore* sem = new Semaphore(lockFile);
+
+    if (sem->test()) {
+        std::ifstream os(lockFile.localPath());
+        std::string s;
+        os >> s;
+        throw SeriousBug("Application " + name() + " is already running as pid " + s);
+    }
+
+    sem->lock();  // OS should reset the semaphore
+
+    std::ofstream os(lockFile.localPath());
+    os << getpid();
+}
+
+//-----------------------------------------------------------------------------
+
+time_t Application::uptime() {
+    long taskId = Context::instance().self();
+
+    Monitor::TaskArray& info = Monitor::instance().tasks();
+    time_t uptime = info[taskId].start();
+    time_t now = ::time(0);
+
+    return now - uptime;
+}
+
+//-----------------------------------------------------------------------------
+
+Application* Application::instance_;
+
+//-----------------------------------------------------------------------------
+
+}  // namespace eckit

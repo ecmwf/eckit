8eaa1b2a339a2238e10ee8b03ac38b2da0917802
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-10 14:04:24 +0100

    MARS-464 Add ManCmd

---
+++ src/eckit/cmd/ManCmd.cc
@@ -0,0 +1,51 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#include "eckit/cmd/ManCmd.h"
+
+//-----------------------------------------------------------------------------
+
+namespace eckit {
+
+//-----------------------------------------------------------------------------
+
+static ManCmd l;
+
+//-----------------------------------------------------------------------------
+
+ManCmd::ManCmd() : CmdResource("man,help") {
+}
+
+//-----------------------------------------------------------------------------
+
+ManCmd::~ManCmd() {
+}
+
+//-----------------------------------------------------------------------------
+
+void ManCmd::execute(std::istream&, std::ostream& out, CmdArg& args) {
+    CmdResource::help(out, args(1, ""));
+}
+
+//-----------------------------------------------------------------------------
+
+void ManCmd::help(std::ostream& out) const {
+    out << "manual pages for commands";
+}
+
+//-----------------------------------------------------------------------------
+
+Arg ManCmd::usage(const std::string& cmd) const {
+    return ~Arg("<command>");
+}
+
+//-----------------------------------------------------------------------------
+
+}  // namespace eckit

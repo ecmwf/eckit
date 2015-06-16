ba60128872c0f951ee9a85854ac8ec085658d117
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-16 16:23:19 +0100

    ECKIT-57 Declare linear algebra types

---
+++ experimental/eckit/la/types.h
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/// @file   types.h
+/// @author Florian Rathgeber
+/// @date   June 2015
+
+#ifndef eckit_la_types_h
+#define eckit_la_types_h
+
+namespace eckit {
+namespace la {
+
+typedef double Scalar;
+class Vector;
+class Matrix;
+
+}  // namespace la
+}  // namespace eckit
+
+#endif

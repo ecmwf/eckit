ad07eab7f1520d9dcc40b445c9d550246d7345a8
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-16 16:37:42 +0100

    ECKIT-57 Add CUDA linear algebra backend

---
+++ experimental/eckit/la/LinearAlgebraCUDA.cc
@@ -0,0 +1,155 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#include "eckit/eckit_config.h"
+
+#ifdef ECKIT_HAVE_CUDA
+
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+//-----------------------------------------------------------------------------
+
+#define CALL_CUDA(e) \
+{ cudaError_t error; \
+    if( ( error = e ) != cudaSuccess ) printf("%s failed with error code %d @ %s +%d\n", #e, error, __FILE__, __LINE__), exit(EXIT_FAILURE); \
+}
+
+#define CALL_CUBLAS(e) \
+{ cublasStatus_t error; \
+    if( ( error = e ) != CUBLAS_STATUS_SUCCESS ) printf("%s failed with error code %d @ %s +%d\n", #e, error, __FILE__, __LINE__), exit(EXIT_FAILURE); \
+}
+
+//-----------------------------------------------------------------------------
+
+#include "eckit/la/LinearAlgebraCUDA.h"
+#include "eckit/la/Matrix.h"
+#include "eckit/la/Vector.h"
+
+//-----------------------------------------------------------------------------
+
+namespace eckit {
+namespace la {
+
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+
+Scalar LinearAlgebraCUDA::dot(const Vector& x, const Vector& y) const {
+    ASSERT( x.size() == y.size() );
+    const size_t size = x.size()*sizeof(Scalar);
+    Scalar r;
+
+    Scalar* d_x; ///< device memory vector x
+    Scalar* d_y; ///< device memory vector y
+    cublasHandle_t handle;
+
+    CALL_CUDA( cudaMalloc((void**) &d_x, size) );
+    CALL_CUDA( cudaMalloc((void**) &d_y, size) );
+
+    CALL_CUBLAS( cublasCreate(&handle) );
+
+    CALL_CUDA( cudaMemcpy(d_x, x.data(), size, cudaMemcpyHostToDevice) );
+    CALL_CUDA( cudaMemcpy(d_y, y.data(), size, cudaMemcpyHostToDevice) );
+
+    CALL_CUBLAS( cublasDdot(handle, x.size(), d_x, 1, d_y, 1, &r) );
+
+    CALL_CUBLAS( cublasDestroy(handle) );
+
+    CALL_CUDA( cudaFree(d_x) );
+    CALL_CUDA( cudaFree(d_y) );
+
+    return r;
+}
+
+//-----------------------------------------------------------------------------
+
+Vector LinearAlgebraCUDA::gemv(const Matrix& A, const Vector& x) const {
+    ASSERT( x.size() == A.cols() );
+    const size_t sizeA = A.rows()*A.cols()*sizeof(Scalar);
+    const size_t sizex = A.cols()*sizeof(Scalar);
+    const size_t sizey = A.rows()*sizeof(Scalar);
+    Vector y(A.rows());
+
+    Scalar* d_A; ///< device memory matrix A
+    Scalar* d_x; ///< device memory vector x
+    Scalar* d_y; ///< device memory vector y
+    cublasHandle_t handle;
+
+    CALL_CUDA( cudaMalloc((void**) &d_A, sizeA) );
+    CALL_CUDA( cudaMalloc((void**) &d_x, sizex) );
+    CALL_CUDA( cudaMalloc((void**) &d_y, sizey) );
+
+    CALL_CUBLAS( cublasCreate(&handle) );
+
+    CALL_CUDA( cudaMemcpy(d_A, A.data(), sizeA, cudaMemcpyHostToDevice) );
+    CALL_CUDA( cudaMemcpy(d_x, x.data(), sizex, cudaMemcpyHostToDevice) );
+
+    const Scalar alpha = 1.0;
+    const Scalar beta  = 0.0;
+    CALL_CUBLAS( cublasDgemv(handle, CUBLAS_OP_N, A.rows(), A.cols(),
+                             &alpha, d_A, A.rows(), d_x, 1, &beta, d_y, 1) );
+
+    CALL_CUDA( cudaMemcpy(y.data(), d_y, sizey, cudaMemcpyDeviceToHost) );
+
+    CALL_CUBLAS( cublasDestroy(handle) );
+
+    CALL_CUDA( cudaFree(d_A) );
+    CALL_CUDA( cudaFree(d_x) );
+    CALL_CUDA( cudaFree(d_y) );
+
+    return y;
+}
+
+//-----------------------------------------------------------------------------
+
+Matrix LinearAlgebraCUDA::gemm(const Matrix& A, const Matrix& B) const {
+    ASSERT( A.cols() == B.rows() );
+    const size_t sizeA = A.rows()*A.cols()*sizeof(Scalar);
+    const size_t sizeB = B.rows()*B.cols()*sizeof(Scalar);
+    const size_t sizeC = A.rows()*B.cols()*sizeof(Scalar);
+    Matrix C(A.rows(), B.cols());
+
+    Scalar* d_A; ///< device memory matrix A
+    Scalar* d_B; ///< device memory matrix B
+    Scalar* d_C; ///< device memory matrix C
+    cublasHandle_t handle;
+
+    CALL_CUDA( cudaMalloc((void**) &d_A, sizeA) );
+    CALL_CUDA( cudaMalloc((void**) &d_B, sizeB) );
+    CALL_CUDA( cudaMalloc((void**) &d_C, sizeC) );
+
+    CALL_CUBLAS( cublasCreate(&handle) );
+
+    CALL_CUDA( cudaMemcpy(d_A, A.data(), sizeA, cudaMemcpyHostToDevice) );
+    CALL_CUDA( cudaMemcpy(d_B, B.data(), sizeB, cudaMemcpyHostToDevice) );
+
+    const Scalar alpha = 1.0;
+    const Scalar beta  = 0.0;
+    CALL_CUBLAS( cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, A.rows(), B.cols(), A.cols(),
+                             &alpha, d_A, A.rows(), d_B, B.rows(), &beta, d_C, A.rows()) );
+
+    CALL_CUDA( cudaMemcpy(C.data(), d_C, sizeC, cudaMemcpyDeviceToHost) );
+
+    CALL_CUBLAS( cublasDestroy(handle) );
+
+    CALL_CUDA( cudaFree(d_A) );
+    CALL_CUDA( cudaFree(d_B) );
+    CALL_CUDA( cudaFree(d_C) );
+
+    return C;
+}
+
+//-----------------------------------------------------------------------------
+
+}  // namespace la
+}  // namespace eckit
+
+#endif  // ECKIT_HAVE_CUDA

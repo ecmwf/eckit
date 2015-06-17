00753ffc10fe6d2817ea9316167fefc9824c3a86
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-06-17 15:00:05 +0100

    MIR-84: moved float compare extra functions to eckit

--- src/eckit/geometry/Point2.cc
+++ src/eckit/geometry/Point2.cc
@@ -1,6 +1,7 @@
+#include "eckit/geometry/Point2.h"
+
 #include <vector>
 
-#include "eckit/geometry/Point2.h"
 #include "eckit/types/FloatCompare.h"
 
 //------------------------------------------------------------------------------------------------------
@@ -12,10 +13,9 @@ namespace geometry {
 
 void reduceTo2Pi( double& d )
 {
-   // See ECKIT-25
-   // while( d < 0 )  d += 360;
-   while( d < -360 ) d += 360;
-	while( d >= 360 ) d -= 360;
+    // See ECKIT-25 // while( d < 0 )  d += 360;
+    while( d < -360 ) d += 360;
+    while( d >= 360 ) d -= 360;
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -31,7 +31,7 @@ std::ostream& operator<<(std::ostream& s,const Point2& p)
 
 bool points_equal(const Point2 &a, const Point2 &b)
 {
-    return FloatCompare::is_equal( Point2::distance2(a,b), 0.0 );
+    return FloatCompare<double>::isApproxEqual( Point2::distance2(a,b), 0.0 );
 }
 
 Point2::operator Value() const
--- src/eckit/types/FloatCompare.h
+++ src/eckit/types/FloatCompare.h
@@ -1,9 +1,22 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Tiago Quintino
+/// @author Pedro Maciel
+/// @date Jun 2015
+
 #ifndef eckit_FloatCompare_h
 #define eckit_FloatCompare_h
 
 #include <limits>
 
-//---------------------------------------------------------------------------------------------------------
 
 namespace eckit {
 
@@ -31,40 +44,55 @@ namespace eckit {
 ///     Grib1: epsilon  0.001
 ///     Grib2: precision is user defined, but generally we can rely on using epsilon = 1e-6  ?
 
+
+/// This function only has specializations for double and float
+/// So it restricts the use of FloatCompare
+template< typename T >
+bool isApproxEqualUlps( T a, T b, T epsilon = std::numeric_limits<T>::epsilon(), int maxUlpsDiff = 10 );
+
+
+/// Comparison class for Floating Point numbers
+/// Type T should be either float or double
+template< typename T >
 class FloatCompare {
 public:
 
-	static bool is_equal( float a,
-						  float b,
-						  float epsilon = std::numeric_limits<float>::epsilon(),
-						  int maxUlpsDiff = 10 );
+	/// Compare values equality
+	static bool isEqual(const T& a,  const T& b)  { return (a == b); }
 
-	static bool is_equal( double a,
-						  double b,
-						  double epsilon = std::numeric_limits<double>::epsilon(),
-						  int maxUlpsDiff = 10 );
+  /// Compare values inequality: "is greater or equal to"
+  static bool isGreaterEqual(const T& a,  const T& b) { return (a >= b); }
+
+  /// Compare values inequality: "is greater or approximately equal to"
+  static bool isGreaterApproxEqual(const T& a,  const T& b) { return (a >= b) || isApproxEqual(a,b); }
+
+	/// Compare values equality, approximately
+	static bool isApproxEqual( T a,
+							T b,
+							T epsilon = std::numeric_limits<float>::epsilon(),
+							int maxUlpsDiff = 10 ) { return isApproxEqualUlps(a,b,epsilon,maxUlpsDiff); }
 
 };
 
-//---------------------------------------------------------------------------------------------------------
 
 /// Helper class that memorizes the comparison setttings
 template < typename T >
-class RealCompare {
+class FloatApproxCompare {
 
 	T eps_;
 	int maxUlps_;
 
 public:
 
-	RealCompare( T eps, int maxUlps = 10 ) :
+	FloatApproxCompare( T eps = std::numeric_limits<T>::epsilon(), int maxUlps = 10 ) :
 		eps_(eps),
 		maxUlps_(maxUlps)
 	{}
 
-	bool operator() ( const T& a,  const T& b) { return FloatCompare::is_equal(a,b,eps_,maxUlps_); }
+	bool operator() ( const T& a,  const T& b) { return FloatCompare<T>::isApproxEqual(a,b,eps_,maxUlps_); }
 
 };
+
 
 //---------------------------------------------------------------------------------------------------------
 

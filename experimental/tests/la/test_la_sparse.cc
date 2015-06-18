da43bc7cf7f882296805fcda160cf37755f372b6
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-18 10:20:50 +0100

    ECKIT-57 Unit test SparseMatrix set from triplets

---
+++ experimental/tests/la/test_la_sparse.cc
@@ -0,0 +1,91 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#define BOOST_TEST_MODULE test_eckit_la_linalg
+
+#include "ecbuild/boost_test_framework.h"
+
+#include "eckit/runtime/Context.h"
+#include "eckit/config/Resource.h"
+
+#include "eckit/la/LinearAlgebraFactory.h"
+#include "eckit/la/SparseMatrix.h"
+
+//-----------------------------------------------------------------------------
+
+using namespace eckit::la;
+
+namespace eckit {
+namespace test {
+
+//-----------------------------------------------------------------------------
+
+// Required for Resources to be initialised
+struct Setup {
+    Setup() {
+        Context::instance().setup(boost::unit_test::framework::master_test_suite().argc,
+                                  boost::unit_test::framework::master_test_suite().argv);
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(Setup);
+
+//-----------------------------------------------------------------------------
+
+struct Fixture {
+
+    Fixture() : S(3, 3),
+                linalg(LinearAlgebraFactory::get(Resource<std::string>("--backend", "generic"))) {
+        std::vector<Triplet> triplets;
+        triplets.push_back(Triplet(0, 0, 2.));
+        triplets.push_back(Triplet(0, 2, -3.));
+        triplets.push_back(Triplet(1, 1, 2.));
+        triplets.push_back(Triplet(2, 2, 2.));
+        S.setFromTriplets(triplets);
+    }
+
+    SparseMatrix S;
+    LinearAlgebraBase* linalg;
+};
+
+//-----------------------------------------------------------------------------
+
+template <class T>
+void test(const T& v, const T& r) {
+    BOOST_CHECK_EQUAL_COLLECTIONS(v.data(), v.data() + v.size(), r.data(), r.data() + r.size());
+}
+
+template <typename T>
+void test(T* v, T* r, size_t s) {
+    BOOST_CHECK_EQUAL_COLLECTIONS(v, v + s, r, r + s);
+}
+
+//-----------------------------------------------------------------------------
+
+/// Test linear algebra interface
+
+BOOST_FIXTURE_TEST_SUITE(test_eckit_la_sparse, Fixture)
+
+BOOST_AUTO_TEST_CASE(test_create_sparse_from_triplets) {
+    BOOST_CHECK_EQUAL(S.nnz(), 4);
+    Index outer[4] = {0, 2, 3, 4};
+    Index inner[4] = {0, 2, 1, 2};
+    Scalar data[4] = {2., -3., 2., 2.};
+    test(S.outer(), outer, 4);
+    test(S.inner(), inner, 4);
+    test(S.data(), data, 4);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+//-----------------------------------------------------------------------------
+
+}  // namespace test
+}  // namespace eckittest

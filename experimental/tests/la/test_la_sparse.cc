a5d90427f885e83bdee42ebb407769f033421711
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-06-24 15:53:29 +0100

    ECKIT-57 Utility method to build a SparseMatrix for testing

--- experimental/tests/la/test_la_sparse.cc
+++ experimental/tests/la/test_la_sparse.cc
@@ -28,6 +28,24 @@ namespace test {
 
 //-----------------------------------------------------------------------------
 
+SparseMatrix S(Size rows, Size cols, Size nnz, ...) {
+    SparseMatrix mat(rows, cols);
+    va_list args;
+    va_start(args, nnz);
+    std::vector<Triplet> triplets;
+    for (Size n = 0; n < nnz; ++n) {
+        Index row = va_arg(args, Index);
+        Index col = va_arg(args, Index);
+        Scalar v = va_arg(args, Scalar);
+        triplets.push_back(Triplet(row, col, v));
+    }
+    va_end(args);
+    mat.setFromTriplets(triplets);
+    return mat;
+}
+
+//-----------------------------------------------------------------------------
+
 // Required for Resources to be initialised
 struct Setup {
     Setup() {
@@ -42,18 +60,15 @@ BOOST_GLOBAL_FIXTURE(Setup);
 
 struct Fixture {
 
-    Fixture() : S(3, 3),
+    Fixture() : A(S(3, 3, 4,
+                    0, 0, 2.,
+                    0, 2, -3.,
+                    1, 1, 2.,
+                    2, 2, 2.)),
                 x(V(3, 1., 2., 3.)),
-                linalg(LinearAlgebraFactory::get()) {
-        std::vector<Triplet> triplets;
-        triplets.push_back(Triplet(0, 0, 2.));
-        triplets.push_back(Triplet(0, 2, -3.));
-        triplets.push_back(Triplet(1, 1, 2.));
-        triplets.push_back(Triplet(2, 2, 2.));
-        S.setFromTriplets(triplets);
-    }
+                linalg(LinearAlgebraFactory::get()) {}
 
-    SparseMatrix S;
+    SparseMatrix A;
     Vector x;
     const LinearAlgebraBase* linalg;
 };
@@ -77,53 +92,53 @@ void test(T* v, T* r, size_t s) {
 
 BOOST_FIXTURE_TEST_SUITE(test_eckit_la_sparse, Fixture)
 
-BOOST_AUTO_TEST_CASE(test_create_sparse_from_triplets) {
-    BOOST_CHECK_EQUAL(S.nonZeros(), 4);
+BOOST_AUTO_TEST_CASE(test_set_from_triplets) {
+    BOOST_CHECK_EQUAL(A.nonZeros(), 4);
     Index outer[4] = {0, 2, 3, 4};
     Index inner[4] = {0, 2, 1, 2};
     Scalar data[4] = {2., -3., 2., 2.};
-    test(S.outer(), outer, 4);
-    test(S.inner(), inner, 4);
-    test(S.data(), data, 4);
+    test(A.outer(), outer, 4);
+    test(A.inner(), inner, 4);
+    test(A.data(), data, 4);
 }
 
 BOOST_AUTO_TEST_CASE(test_identity) {
     Vector y1(3);
-    SparseMatrix A(3, 3);
-    A.setIdentity();
-    linalg->spmv(A, x, y1);
-    test(y1, x);
-    SparseMatrix B(6, 3);
+    SparseMatrix B(3, 3);
     B.setIdentity();
+    linalg->spmv(B, x, y1);
+    test(y1, x);
+    SparseMatrix C(6, 3);
+    C.setIdentity();
     Vector y2(6);
-    linalg->spmv(B, x, y2);
+    linalg->spmv(C, x, y2);
     test(y2, x);
     test(y2.data()+3, V(3, 0., 0., 0.).data(), 3);
-    SparseMatrix C(2, 3);
-    C.setIdentity();
+    SparseMatrix D(2, 3);
+    D.setIdentity();
     Vector y3(2);
-    linalg->spmv(C, x, y3);
+    linalg->spmv(D, x, y3);
     test(y3, x);
 }
 
 BOOST_AUTO_TEST_CASE(test_prune) {
-    S.data()[1] = 0.;
-    S.prune();
-    BOOST_CHECK_EQUAL(S.nonZeros(), 3);
+    A.data()[1] = 0.;
+    A.prune();
+    BOOST_CHECK_EQUAL(A.nonZeros(), 3);
     Index outer[4] = {0, 1, 2, 3};
     Index inner[3] = {0, 1, 2};
     Scalar data[3] = {2., 2., 2.};
-    test(S.outer(), outer, 4);
-    test(S.inner(), inner, 3);
-    test(S.data(), data, 3);
+    test(A.outer(), outer, 4);
+    test(A.inner(), inner, 3);
+    test(A.data(), data, 3);
 }
 
 BOOST_AUTO_TEST_CASE(test_spmv) {
     Vector y(3);
-    linalg->spmv(S, x, y);
+    linalg->spmv(A, x, y);
     test(y, V(3, -7., 4., 6.));
     BOOST_TEST_MESSAGE("spmv of sparse matrix and vector of nonmatching sizes should fail");
-    BOOST_CHECK_THROW(linalg->spmv(S, Vector(2), y), AssertionFailed);
+    BOOST_CHECK_THROW(linalg->spmv(A, Vector(2), y), AssertionFailed);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

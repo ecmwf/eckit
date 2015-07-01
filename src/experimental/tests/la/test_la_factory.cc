63b704ddbdb3bd14d6218bdda720a5791c9bef29
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-07-01 15:34:33 +0100

    ECKIT-64 Unit test LinearAlgebraFactory

---
+++ src/experimental/tests/la/test_la_factory.cc
@@ -0,0 +1,73 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/// @file   test_la_factory.cc
+/// @author Florian Rathgeber
+/// @date   July 2015
+
+#define BOOST_TEST_MODULE test_eckit_la_factory
+
+#include "ecbuild/boost_test_framework.h"
+
+#include "eckit/eckit_config.h"
+#include "experimental/eckit/la/LinearAlgebraFactory.h"
+
+//-----------------------------------------------------------------------------
+
+using namespace eckit::la;
+
+namespace eckit {
+namespace test {
+
+/// Test linear algebra factory
+
+BOOST_AUTO_TEST_SUITE(test_eckit_la_factory)
+
+BOOST_AUTO_TEST_CASE(test_list) {
+    std::ostringstream oss;
+    LinearAlgebraFactory::list(oss);
+    BOOST_TEST_MESSAGE("Available linear algebra backends: " << oss.str());
+    BOOST_CHECK_NE(oss.str().find("generic"), std::string::npos);
+#ifdef ECKIT_HAVE_ARMADILLO
+    BOOST_CHECK_NE(oss.str().find("armadillo"), std::string::npos);
+#endif
+#ifdef ECKIT_HAVE_CUDA
+    BOOST_CHECK_NE(oss.str().find("cuda"), std::string::npos);
+#endif
+#ifdef ECKIT_HAVE_EIGEN
+    BOOST_CHECK_NE(oss.str().find("eigen"), std::string::npos);
+#endif
+#ifdef ECKIT_HAVE_MKL
+    BOOST_CHECK_NE(oss.str().find("mkl"), std::string::npos);
+#endif
+}
+
+BOOST_AUTO_TEST_CASE(test_get) {
+    BOOST_CHECK(LinearAlgebraFactory::get());
+    BOOST_CHECK(LinearAlgebraFactory::get("generic"));
+#ifdef ECKIT_HAVE_ARMADILLO
+    BOOST_CHECK(LinearAlgebraFactory::get("armadillo"));
+#endif
+#ifdef ECKIT_HAVE_CUDA
+    BOOST_CHECK(LinearAlgebraFactory::get("cuda"));
+#endif
+#ifdef ECKIT_HAVE_EIGEN
+    BOOST_CHECK(LinearAlgebraFactory::get("eigen"));
+#endif
+#ifdef ECKIT_HAVE_MKL
+    BOOST_CHECK(LinearAlgebraFactory::get("mkl"));
+#endif
+    BOOST_CHECK_THROW(LinearAlgebraFactory::get("foo"), BadParameter);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace test
+}  // namespace eckit

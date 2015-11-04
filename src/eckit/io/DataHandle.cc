c2a83c4e38e818bed6cd4c808f0cb070aff50391
Florian Rathgeber <florian.rathgeber@ecmwf.int>
2015-11-04 14:31:01 +0000

    Fix uninitialised variables warning

--- src/eckit/io/DataHandle.cc
+++ src/eckit/io/DataHandle.cc
@@ -157,7 +157,7 @@ Length DataHandle::saveInto(DataHandle& other,TransferWatcher& watcher)
                     watcher.watch(buffer,length);
                     lastRead = timer.elapsed();
 
-                    char c1,c2;
+                    char c1 = 0, c2 = 0;
                     //			Log::message() << rate(length/r,c1)  << c1 << " " << rate(length/w,c2) << c2 << std::endl;
                     Log::message() << rate(total/readTime,c1)  << c1 << " " << rate(total/writeTime,c2) << c2 << std::endl;
                 }

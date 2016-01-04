9ac5e71b3262d12cd2dfe9dae3eef693345037ce
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-01-05 00:12:48 +0100

    Port option lib from MIR to eckit

---
+++ src/experimental/eckit/option/Option.h
@@ -0,0 +1,125 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date Apr 2015
+
+
+#ifndef Option_H
+#define Option_H
+
+#include <iosfwd>
+#include <string>
+
+
+namespace eckit {
+
+class LocalConfiguration;
+
+namespace option {
+
+
+class Option {
+  public:
+
+// -- Exceptions
+    // None
+
+// -- Contructors
+
+    Option(const std::string& name, const std::string& description);
+
+// -- Destructor
+
+    virtual ~Option(); // Change to virtual if base class
+
+// -- Convertors
+    // None
+
+// -- Operators
+    // None
+
+// -- Methods
+    // None
+
+    const std::string& name() const;
+
+    virtual bool active() const;
+
+    virtual void set(LocalConfiguration&) const;
+    virtual void set(const std::string& value, LocalConfiguration&) const = 0;
+
+
+// -- Overridden methods
+    // None
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+  protected:
+
+// -- Members
+
+    std::string name_;
+    std::string description_;
+
+// -- Methods
+
+    virtual void print(std::ostream&) const = 0; // Change to virtual if base class
+
+// -- Overridden methods
+    // None
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+  private:
+
+// No copy allowed
+
+    Option(const Option&);
+    Option& operator=(const Option&);
+
+// -- Members
+    // None
+
+// -- Methods
+    // None
+
+// -- Overridden methods
+    // None
+
+// -- Class members
+    // None
+
+// -- Class methods
+    // None
+
+// -- Friends
+
+    friend std::ostream& operator<<(std::ostream& s, const Option& p) {
+        p.print(s);
+        return s;
+    }
+
+};
+
+} // namespace option
+
+} // namespace eckit
+#endif
+
---
+++ src/experimental/eckit/option/SimpleOption.cc
@@ -0,0 +1,94 @@
+/*
+ * (C) Copyright 1996-2015 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date Apr 2015
+
+#include <iostream>
+
+#include "eckit/option/SimpleOption.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/utils/Translator.h"
+#include "eckit/filesystem/PathName.h"
+#include "eckit/config/LocalConfiguration.h"
+
+namespace eckit {
+
+namespace option {
+
+template<class T> struct Title {
+    const char *operator()() const;
+};
+
+template<>
+const char *Title<size_t>::operator()() const {
+    return "ordinal";
+}
+
+template<>
+const char *Title<long>::operator()() const {
+    return "interger";
+}
+
+template<>
+const char *Title<double>::operator()() const {
+    return "real";
+}
+
+template<>
+const char *Title<bool>::operator()() const {
+    return "0/1";
+}
+
+template<>
+const char *Title<std::string>::operator()() const {
+    return "string";
+}
+
+template<>
+const char *Title<eckit::PathName>::operator()() const {
+    return "path";
+}
+
+template<class T>
+SimpleOption<T>::SimpleOption(const std::string &name, const std::string &description):
+    Option(name, description) {
+}
+
+template<class T>
+SimpleOption<T>::~SimpleOption() {
+}
+
+template<class T>
+void SimpleOption<T>::set(const std::string &value, LocalConfiguration &parametrisation) const {
+    T v = eckit::Translator<std::string, T>()(value);
+    parametrisation.set(name_, v);
+}
+
+template<class T>
+void SimpleOption<T>::set(LocalConfiguration &parametrisation) const {
+    Option::set(parametrisation);
+}
+
+template<>
+void SimpleOption<bool>::set(LocalConfiguration &parametrisation) const {
+    parametrisation.set(name_, true);
+}
+
+template<class T>
+void SimpleOption<T>::print(std::ostream &out) const {
+    out << "   --" << name_ << "=" << Title<T>()() << " (" << description_ << ")";
+}
+
+} // namespace option
+
+} // namespace eckit
+

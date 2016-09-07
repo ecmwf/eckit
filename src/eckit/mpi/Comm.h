a44de90a8733d234264b3625ae37fc1e7c77687b
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-09-07 09:10:05 +0100

    ECKIT-158

--- src/eckit/mpi/Comm.h
+++ src/eckit/mpi/Comm.h
@@ -415,12 +415,10 @@ public:  // methods
     }
 
     ///
-    ///
+    /// All to all of vector< vector<> >
     ///
     template <typename T>
-    void allToAll(const std::vector< std::vector<T> >& sendvec, std::vector< std::vector<T> >& recvvec) const {
-        eckit::mpi::Collectives<T>::allToAll(*this, sendvec, recvvec);
-    }
+    void allToAll(const std::vector< std::vector<T> >& sendvec, std::vector< std::vector<T> >& recvvec) const;
 
 private: // methods
 
@@ -472,7 +470,90 @@ class CommBuilder : public CommFactory {
 
 //----------------------------------------------------------------------------------------------------------------------
 
-#include "eckit/mpi/Collectives.h"
+#if 0
+
+///
+/// All reduce a pair
+///
+
+static void allReduce(const eckit::mpi::Comm& comm, const std::pair<T,int>& send, std::pair<T,int>& recv, Operation::Code op)  {
+    NOTIMP;
+}
+
+///
+/// All reduce a vector of pairs
+///
+
+static void allReduce(const eckit::mpi::Comm& comm, const std::vector< std::pair<T,int> >& send, std::vector< std::pair<T,int> >& recv, Operation::Code op)  {
+    NOTIMP;
+}
+
+#endif
+
+template <typename T>
+void eckit::mpi::Comm::allToAll(const std::vector< std::vector<T> >& sendvec, std::vector< std::vector<T> >& recvvec) const {
+
+    size_t mpi_size = size();
+
+    ASSERT( sendvec.size() == mpi_size );
+    ASSERT( recvvec.size() == mpi_size );
+
+    // Get send-information
+    std::vector<int> sendcounts(mpi_size);
+    std::vector<int> senddispls(mpi_size);
+    int sendcnt;
+    senddispls[0] = 0;
+    sendcounts[0] = sendvec[0].size();
+    sendcnt = sendcounts[0];
+
+    for(size_t jproc = 1; jproc < mpi_size; ++jproc)
+    {
+        senddispls[jproc] = senddispls[jproc-1] + sendcounts[jproc-1];
+        sendcounts[jproc] = sendvec[jproc].size();
+        sendcnt += sendcounts[jproc];
+    }
+
+
+    // Get recv-information
+    std::vector<int> recvcounts(mpi_size);
+    std::vector<int> recvdispls(mpi_size);
+    int recvcnt;
+
+    allToAll(sendcounts.data(), 1, recvcounts.data(), 1);
+
+    recvdispls[0] = 0;
+    recvcnt = recvcounts[0];
+    for(size_t jproc = 1; jproc < mpi_size; ++jproc)
+    {
+        recvdispls[jproc] = recvdispls[jproc-1] + recvcounts[jproc-1];
+        recvcnt += recvcounts[jproc];
+    }
+
+    // Communicate
+    std::vector<T> sendbuf(sendcnt);
+    std::vector<T> recvbuf(recvcnt);
+    int cnt = 0;
+    for(size_t jproc = 0; jproc < mpi_size; ++jproc)
+    {
+        for(int i = 0; i < sendcounts[jproc]; ++i)
+        {
+            sendbuf[cnt++] = sendvec[jproc][i];
+        }
+    }
+
+    allToAllv( sendbuf.data(), sendcounts.data(), senddispls.data(),
+               recvbuf.data(), recvcounts.data(), recvdispls.data());
+
+    cnt=0;
+    for(size_t jproc = 0; jproc < mpi_size; ++jproc)
+    {
+        recvvec[jproc].resize(recvcounts[jproc]);
+        for(int i = 0; i < recvcounts[jproc]; ++i)
+        {
+            recvvec[jproc][i] = recvbuf[cnt++];
+        }
+    }
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 

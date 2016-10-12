587a4103ab97ef19336598c55cd6fb1ef825ca17
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-10-12 19:46:42 +0100

    Add FileLock

---
+++ src/eckit/io/FileLock.cc
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "eckit/eckit.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/io/FileLock.h"
+#include "eckit/os/AutoUmask.h"
+#include "eckit/filesystem/PathName.h"
+
+namespace eckit {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+static int openLock(const PathName& lockFile) {
+    AutoUmask mask(0);
+    int fd;
+    SYSCALL2(fd = ::open(lockFile.asString().c_str(), O_CREAT, 0777), lockFile);
+    return fd;
+}
+
+
+FileLock::FileLock(const PathName& lockFile):
+    fd_(openLock(lockFile)),
+    locker_(fd_)
+{
+}
+
+FileLock::~FileLock()
+{
+    ::close(fd_);
+}
+
+void FileLock::lock()
+{
+    locker_.lockExclusive();
+}
+
+
+void FileLock::unlock()
+{
+    locker_.unlock();
+
+}
+
+//-----------------------------------------------------------------------------
+
+} // namespace eckit

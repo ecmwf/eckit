a6e9cc6ea3fc15daac5d0f46a34096c3952e8757
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-11-28 10:35:45 +0000

    ECKIT-176: add way to force eckit mpi parallel backend

--- src/eckit/mpi/Comm.cc
+++ src/eckit/mpi/Comm.cc
@@ -27,10 +27,10 @@ public:
 
     static bool areMPIVarsSet() {
         return (
-           ::getenv("OMPI_COMM_WORLD_SIZE") || // OpenMPI
-           ::getenv("ALPS_APP_PE")          || // Cray
-           ::getenv("PMI_SIZE")             || // Intel
-           ::getenv("ECKIT_USE_MPI")        ); // If all else fails
+           ::getenv("OMPI_COMM_WORLD_SIZE")   ||  // OpenMPI
+           ::getenv("ALPS_APP_PE")            ||  // Cray PE
+           ::getenv("PMI_SIZE")               ||  // Intel
+           ::getenv("ECKIT_MPI_FORCE_PARALLEL")); // If all else fails
     }
 
     static Environment& instance() {

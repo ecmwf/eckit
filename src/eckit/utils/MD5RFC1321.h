d2b6d28c28251c5bf274bd383d10f48e82853451
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-12-08 15:31:13 +0000

    ECKIT-185: testing alternative implemntation

---
+++ src/eckit/utils/MD5RFC1321.h
@@ -0,0 +1,117 @@
+/*
+ * (C) Copyright 1996-2016 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef eckit_utils_MD5_H
+#define eckit_utils_MD5_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string>
+
+#include "eckit/memory/NonCopyable.h"
+
+namespace eckit {
+
+class MD5 : private eckit::NonCopyable {
+
+public:  // types
+
+  typedef std::string digest_t;
+
+public:  // types
+
+  MD5();
+
+  explicit MD5(const digest_t&);
+
+  MD5(const void* data, size_t len);
+
+  ~MD5();
+
+  void add(const void*, long);
+
+  void add(char x){ add(&x, sizeof(x)); }
+  void add(unsigned char x){ add(&x, sizeof(x)); }
+
+  void add(bool x){ add(&x, sizeof(x)); }
+
+  void add(int x){ add(&x, sizeof(x)); }
+  void add(unsigned int x){ add(&x, sizeof(x)); }
+
+  void add(short x){ add(&x, sizeof(x)); }
+  void add(unsigned short x){ add(&x, sizeof(x)); }
+
+  void add(long x){ add(&x, sizeof(x)); }
+  void add(unsigned long x){ add(&x, sizeof(x)); }
+
+  void add(long long x){ add(&x, sizeof(x)); }
+  void add(unsigned long long x){ add(&x, sizeof(x)); }
+
+  void add(float x){ add(&x, sizeof(x)); }
+  void add(double x){ add(&x, sizeof(x)); }
+
+  void add(const std::string& x)  { add(x.c_str(),x.size()); }
+  void add(const char* x) { add(std::string(x)); }
+  
+  /// for generic objects
+  template <class T>
+  void add(const T& x) { x.hash(*this); }
+
+  template<class T>
+  MD5& operator<<(const T& x) { add(x); return *this; }
+
+  operator std::string();
+
+  digest_t digest() const;
+
+private:  // types
+
+  // Make sure this is not called with a pointer
+  template<class T>
+  void add(const T* x);
+  void add(const void*);
+
+  /// Double hashing
+  void add(const MD5& md5) { add(md5.digest()); }
+
+private: // members
+
+  mutable digest_t digest_;  ///< cached digest
+
+  /* POINTER defines a generic pointer type */
+  typedef unsigned char *POINTER;
+
+  /* UINT2 defines a two byte word */
+  typedef unsigned short int UINT2;
+
+  /* UINT4 defines a four byte word */
+  typedef unsigned long int UINT4;
+
+  /* MD5 context. */
+  typedef struct {
+    UINT4 state[4];                                   /* state (ABCD) */
+    UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
+    unsigned char buffer[64];                         /* input buffer */
+  } MD5_CTX;
+
+  mutable MD5_CTX s_;
+
+  static void Init    (MD5_CTX *);
+  static void Update  (MD5_CTX *, unsigned char *, unsigned int);
+  static void Final   (unsigned char [16], MD5_CTX *);
+  static void Transform (UINT4 [4], unsigned char [64]);
+  static void Encode (unsigned char *, UINT4 *, unsigned int);
+  static void Decode (UINT4 *, unsigned char *, unsigned int);
+
+};
+
+}  // end namespace eckit
+
+#endif
--- src/tests/utils/test_md5.cc
+++ src/tests/utils/test_md5.cc
@@ -66,6 +66,32 @@ BOOST_AUTO_TEST_CASE( test_eckit_utils_md5_double_add )
 	BOOST_CHECK_EQUAL( res , md5.digest() );
 }
 
+// original test suite from RFC-1321
+
+BOOST_AUTO_TEST_CASE( test_eckit_utils_md5_test_suite_from_rfc1321 )
+{
+    BOOST_CHECK_EQUAL( MD5("").digest(),
+                       "d41d8cd98f00b204e9800998ecf8427e" );
+
+    BOOST_CHECK_EQUAL( MD5("a").digest(),
+                       "0cc175b9c0f1b6a831c399e269772661" );
+
+    BOOST_CHECK_EQUAL( MD5("abc").digest(),
+                       "900150983cd24fb0d6963f7d28e17f72" );
+
+    BOOST_CHECK_EQUAL( MD5("message digest").digest(),
+                       "f96b697d7cb7938d525a2f31aaf161d0" );
+
+    BOOST_CHECK_EQUAL( MD5("abcdefghijklmnopqrstuvwxyz").digest(),
+                       "c3fcd3d76192e4007dfb496cca67e13b" );
+
+    BOOST_CHECK_EQUAL( MD5("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789").digest(),
+                       "d174ab98d277d9f5a5611c2c9f419d9f" );
+
+    BOOST_CHECK_EQUAL( MD5("12345678901234567890123456789012345678901234567890123456789012345678901234567890").digest(),
+                       "57edf4a22be3c955ac49da2e2107b67a" );
+}
+
 //-----------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE_END()

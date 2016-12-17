8a594f9d14c4a0f66a03ed56c9b92067a93c46c7
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-12-17 15:59:13 +0000

    Cosmetics changes

--- src/tests/filesystem/test_aiohandle.cc
+++ src/tests/filesystem/test_aiohandle.cc
@@ -20,11 +20,10 @@
 using namespace std;
 using namespace eckit;
 
-//-----------------------------------------------------------------------------
+namespace eckit {
+namespace test {
 
-namespace eckit_test {
-
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
 class TestAIOHandle : public Tool {
 public:
@@ -34,80 +33,77 @@ public:
     ~TestAIOHandle() {}
 
     virtual void run();
-    
+
     void setup();
     void teardown();
 
     void test_write();
     void test_append();
-    
+
     PathName path_;
-    
+
 };
 
-//-----------------------------------------------------------------------------
-            
+
 void TestAIOHandle::test_write()
 {
     DataHandle* aioh = new AIOHandle(path_);
-    
+
     aioh->openForWrite(0);
-    
+
     const char buf [] = "74e1feb8d0b1d328cbea63832c2dcfb2b4fa1adf";
-    
+
     aioh->write(buf,sizeof(buf));
-    
+
     aioh->close();
-    
+
     delete aioh;
-    
+
     DataHandle* fh = path_.fileHandle();
-    
+
     fh->openForRead();
-    
+
     Buffer buf2(1024);
-    
+
     fh->read(buf2,buf2.size());
     fh->close();
-    
+
     delete fh;
-    
+
     ASSERT( buf == string(buf2) );
 }
 
-//-----------------------------------------------------------------------------
-            
+
 void TestAIOHandle::test_append()
 {
     DataHandle* aioh = new AIOHandle(path_);
-    
+
     aioh->openForAppend(0);
-    
+
     const char buf [] = "53d50e63a50fba73f0151028a695a238ff06491c";
-    
+
     aioh->write(buf,sizeof(buf));
-    
+
     aioh->close();
-    
+
     delete aioh;
-    
+
     DataHandle* fh = path_.fileHandle();
-    
+
     fh->openForRead();
-    
+
     fh->seek( sizeof(buf) );
-    
+
     Buffer buf2(1024);
-    
+
     fh->read(buf2,buf2.size());
     fh->close();
-    
+
     delete fh;
-    
+
     ASSERT( buf == string(buf2) );
 }
 
-//-----------------------------------------------------------------------------
 
 void TestAIOHandle::setup()
 {
@@ -118,30 +114,28 @@ void TestAIOHandle::setup()
 
 void TestAIOHandle::teardown()
 {
-    path_.unlink();    
+    path_.unlink();
 }
 
-//-----------------------------------------------------------------------------
-            
+
 void TestAIOHandle::run()
 {
     setup();
-    
+
     test_write();
     test_append();
-    
+
     teardown();
 }
 
-//-----------------------------------------------------------------------------
+} // namespace test
+} // namespace eckit
 
-} // namespace eckit_test
-
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
 int main(int argc,char **argv)
 {
-    eckit_test::TestAIOHandle app(argc,argv);
+    eckit::test::TestAIOHandle app(argc,argv);
     return app.start();
 }
 

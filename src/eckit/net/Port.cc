c001426f21f3e1769fa121be8302a9dc3a618b53
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-08-10 10:27:06 +0200

    Work on tunnel

--- src/eckit/net/Port.cc
+++ src/eckit/net/Port.cc
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2017 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -19,7 +19,7 @@ Port::Port(const std::string& name, int port):
     port_(port)
 {
     int offset = Resource<int>("portOffset",0);
-    port_ = Resource<int>(name + "Port", port) + offset;
+    port_ = Resource<int>(name + "Port;-" + name + "Port", port) + offset;
 }
 
 //----------------------------------------------------------------------------------------------------------------------

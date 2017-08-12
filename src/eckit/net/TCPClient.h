5e2c87930418390f559fa8800691fa92b728e029
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-08-12 17:26:19 +0200

    Add proxy support

--- src/eckit/net/TCPClient.h
+++ src/eckit/net/TCPClient.h
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2017 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -36,7 +36,7 @@ public:
 
 // -- Methods
 
-	TCPSocket& connect(const std::string& host,int port, int retries = 5, int timeout = 0);
+	virtual TCPSocket& connect(const std::string& host,int port, int retries = 5, int timeout = 0);
 
 private:
 
@@ -46,7 +46,7 @@ private:
 	TCPClient& operator=(const TCPClient&);
 
 // -- Members
-	
+
 	int port_;
 
 // -- Overridden methods

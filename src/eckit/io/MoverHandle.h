6c0edb971e9be2a4421c752f2ca661bb33346f6a
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-08-17 22:00:29 +0100

    Use ScopedPtr()

--- src/eckit/io/MoverHandle.h
+++ src/eckit/io/MoverHandle.h
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2017 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -17,6 +17,7 @@
 #include "eckit/io/DataHandle.h"
 #include "eckit/thread/Mutex.h"
 #include "eckit/net/TCPSocket.h"
+#include "eckit/memory/ScopedPtr.h"
 
 
 //-----------------------------------------------------------------------------
@@ -37,7 +38,7 @@ public:
 
 // -- Destructor
 
-	virtual ~MoverHandle(); 
+	virtual ~MoverHandle();
 
 // -- Convertors
 	// None
@@ -65,7 +66,7 @@ protected:
 	// None
 
 // -- Methods
-	
+
 
 // -- Overridden methods
 	// None
@@ -81,7 +82,7 @@ private:
 
 // -- Members
 
-    std::auto_ptr<DataHandle> handle_;
+    eckit::ScopedPtr<DataHandle> handle_;
     TCPSocket data_;
     bool fail_;
     std::string error_;
@@ -96,12 +97,12 @@ private:
     virtual Length openForRead() ;
     virtual void openForWrite(const Length&);
     virtual void openForAppend(const Length&);
-    virtual long read(void*,long); 
+    virtual long read(void*,long);
     virtual long write(const void*,long);
     virtual void close();
     virtual std::string title() const;
 
-	void print(std::ostream&) const; 	
+	void print(std::ostream&) const;
 
 // -- Class members
 	// None

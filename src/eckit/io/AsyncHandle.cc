6b0502e9dc1e14e626240433aa9f314191c30e6e
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-12-09 11:08:14 +0000

    Add AsyncHandle

---
+++ src/eckit/io/AsyncHandle.cc
@@ -0,0 +1,222 @@
+/*
+ * (C) Copyright 1996-2017 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/io/AsyncHandle.h"
+#include "eckit/thread/Thread.h"
+#include "eckit/thread/AutoLock.h"
+#include "eckit/maths/Functions.h"
+
+//-----------------------------------------------------------------------------
+
+namespace eckit {
+
+//-----------------------------------------------------------------------------
+
+#if 0
+ClassSpec AsyncHandle::classSpec_ = {&DataHandle::classSpec(), "AsyncHandle",};
+Reanimator<AsyncHandle> AsyncHandle::reanimator_;
+#endif
+
+class AsyncHandleWriter : public Thread {
+    AsyncHandle& owner_;
+    virtual void run();
+public:
+    AsyncHandleWriter(AsyncHandle& owner) : owner_(owner) {}
+};
+
+void AsyncHandleWriter::run() {
+    while (!stopped()) {
+        try {
+            AutoLock<MutexCond> lock(owner_.cond_);
+            while (owner_.buffers_.empty()) {
+                owner_.cond_.wait();
+            }
+
+            std::pair<size_t, Buffer*> p = owner_.buffers_.front();
+            owner_.buffers_.pop_front();
+            owner_.used_ -= p.second->size();
+
+            long written = owner_.handle().write(p.second->data(), p.first);
+            if(written != p.first) {
+                std::ostringstream oss;
+                oss << "AsyncHandleWriter: written " << written << " out of " << p.first << Log::syserr;
+                throw WriteError(oss.str());
+            }
+
+            owner_.cond_.signal();
+        }
+        catch (std::exception& e) {
+            eckit::Log::error() << "AsyncHandleWriter got an exception: " << e.what() << std::endl;
+            AutoLock<MutexCond> lock(owner_.cond_);
+            owner_.error_ = e.what();
+            owner_.cond_.signal();
+        }
+    }
+}
+
+AsyncHandle::AsyncHandle(DataHandle* h, size_t maxSize, size_t rounding):
+    HandleHolder(h),
+    maxSize_(maxSize),
+    rounding_(rounding),
+    used_(0),
+    error_(false),
+    thread_(new AsyncHandleWriter(*this))
+{
+}
+
+AsyncHandle::AsyncHandle(DataHandle& h, size_t maxSize, size_t rounding):
+    HandleHolder(h),
+    maxSize_(maxSize),
+    rounding_(rounding),
+    used_(0),
+    error_(false),
+    thread_(new AsyncHandleWriter(*this))
+{
+}
+
+AsyncHandle::~AsyncHandle()
+{
+    {
+        AutoLock<MutexCond> lock(cond_);
+        while (!buffers_.empty()) {
+            delete buffers_.front().second;
+            buffers_.pop_front();
+        }
+        cond_.signal();
+    }
+
+    thread_.stop();
+    thread_.wait();
+}
+
+Length AsyncHandle::openForRead()
+{
+    NOTIMP;
+}
+
+void AsyncHandle::openForWrite(const Length& length)
+{
+    ASSERT(used_ == 0);
+    ASSERT(buffers_.size() == 0);
+    handle().openForWrite(length);
+}
+
+void AsyncHandle::openForAppend(const Length& length)
+{
+    ASSERT(used_ == 0);
+    ASSERT(buffers_.size() == 0);
+    handle().openForAppend(length);
+}
+
+void AsyncHandle::skip(const Length& len)
+{
+    NOTIMP;
+}
+
+long AsyncHandle::read(void* buffer, long length)
+{
+    NOTIMP;
+}
+
+long AsyncHandle::write(const void* buffer, long length)
+{
+    AutoLock<MutexCond> lock(cond_);
+
+    size_t size = eckit::round(length, rounding_);
+
+    while (used_ + size >= maxSize_ && !error_) {
+        // Special case for size > maxSize_
+        if (buffers_.empty()) {
+            break;
+        }
+        cond_.wait();
+    }
+
+    if (error_) {
+        throw WriteError(message_);
+    }
+
+    std::pair<size_t, Buffer*> p(length, new Buffer(size));
+    ::memcpy(p.second->data(), buffer, length);
+    buffers_.push_back(p);
+    used_ += size;
+
+    cond_.signal();
+
+    return length;
+}
+
+void AsyncHandle::close()
+{
+    flush();
+    handle().close();
+
+    AutoLock<MutexCond> lock(cond_);
+
+    if (error_) {
+        throw WriteError(message_);
+    }
+}
+
+void AsyncHandle::flush()
+{
+    AutoLock<MutexCond> lock(cond_);
+    while (!buffers_.empty() && !error_) {
+        cond_.wait();
+    }
+
+    if (error_) {
+        throw WriteError(message_);
+    }
+
+    handle().flush();
+}
+
+void AsyncHandle::rewind()
+{
+    NOTIMP;
+}
+
+Offset AsyncHandle::seek(const Offset& off)
+{
+    NOTIMP;
+}
+
+
+void AsyncHandle::print(std::ostream& s) const
+{
+    s << "AsyncHandle[";
+    handle().print(s);
+    s << ']';
+}
+
+Length AsyncHandle::estimate()
+{
+    return handle().estimate();
+}
+
+Offset AsyncHandle::position()
+{
+    NOTIMP;
+}
+
+std::string AsyncHandle::title() const {
+    return std::string("{") + handle().title() + "}";
+}
+
+DataHandle* AsyncHandle::clone() const
+{
+    return new AsyncHandle(handle().clone(), maxSize_, rounding_);
+}
+//-----------------------------------------------------------------------------
+
+} // namespace eckit
+
---
+++ src/eckit/io/AsyncHandle.h
@@ -0,0 +1,116 @@
+/*
+ * (C) Copyright 1996-2017 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+// File io/AsyncHandle.h
+// Baudouin Raoult - ECMWF Dec 17
+
+#ifndef eckit_io_AsyncHandle_h
+#define eckit_io_AsyncHandle_h
+
+#include "eckit/io/Buffer.h"
+#include "eckit/io/HandleHolder.h"
+#include "eckit/thread/ThreadControler.h"
+#include "eckit/thread/MutexCond.h"
+#include "eckit/io/Buffer.h"
+
+//-----------------------------------------------------------------------------
+
+namespace eckit {
+
+//-----------------------------------------------------------------------------
+
+class AsyncHandle : public DataHandle, public HandleHolder {
+public:
+
+
+    /// Contructor, taking ownership
+
+    AsyncHandle(DataHandle*, size_t = 1024 * 1024, size_t rounding = 64 * 1024);
+
+    /// Contructor, not taking ownership
+
+    AsyncHandle(DataHandle&, size_t = 1024 * 1024, size_t rounding = 64 * 1024);
+
+    /// Destructor
+
+    virtual ~AsyncHandle();
+
+// -- Operators
+
+
+// -- Overridden methods
+
+    // From DataHandle
+
+    virtual Length openForRead();
+    virtual void openForWrite(const Length&);
+    virtual void openForAppend(const Length&);
+
+    virtual long read(void*, long);
+    virtual long write(const void*, long);
+    virtual void close();
+    virtual void flush();
+    virtual void rewind();
+    virtual void print(std::ostream&) const;
+    virtual void skip(const Length&);
+
+    virtual Offset seek(const Offset&);
+
+    virtual Length estimate();
+    virtual Offset position();
+
+    virtual DataHandle* clone() const;
+
+    // From Streamable
+
+#if 0
+    virtual void encode(Stream&) const;
+    virtual const ReanimatorBase& reanimator() const { return reanimator_; }
+#endif
+
+// -- Class methods
+
+#if 0
+    static  const ClassSpec&  classSpec()        { return classSpec_;}
+#endif
+
+private: // methods
+
+
+private: // members
+
+    bool               error_;
+    std::string        message_;
+    size_t             maxSize_;
+    size_t             used_;
+    size_t             rounding_;
+    MutexCond          cond_;
+    std::deque<std::pair<size_t,Buffer*> > buffers_;
+    ThreadControler    thread_; // must be last
+
+    virtual std::string title() const;
+
+// -- Class members
+
+    friend class AsyncHandleWriter;
+
+#if 0
+    static  ClassSpec                 classSpec_;
+    static  Reanimator<AsyncHandle>  reanimator_;
+#endif
+
+};
+
+//-----------------------------------------------------------------------------
+
+} // namespace eckit
+
+
+#endif

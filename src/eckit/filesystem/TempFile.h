198836983b56245784edbc38650df53fbd4379e8
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-02-15 11:45:14 +0000

    Cosmetics

--- src/eckit/filesystem/TempFile.h
+++ src/eckit/filesystem/TempFile.h
@@ -1,15 +1,15 @@
 /*
  * (C) Copyright 1996-2017 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
-// File TempFile.h
-// Baudouin Raoult - ECMWF Nov 96
+/// @author Baudouin Raoult
+/// @date   Nov 1996
 
 #ifndef eckit_TempFile_h
 #define eckit_TempFile_h
@@ -17,34 +17,23 @@
 #include "eckit/filesystem/PathName.h"
 
 
-//-----------------------------------------------------------------------------
-
 namespace eckit {
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
 class TempFile : public PathName {
 public:
+    TempFile();
+    TempFile(const std::string&); /* the std::string is the name of a filespace */
 
-// -- Contructors
-
-	TempFile();
-	TempFile(const std::string&); /* the std::string is the name of a filespace */
-
-// -- Destructor
-
-	~TempFile();
+    ~TempFile();
 
 private:
-
-// -- Class member
-
-	static PathName dir(const std::string&);
+    static PathName dir(const std::string&);
 };
 
+//----------------------------------------------------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-
-} // namespace eckit
+}  // namespace eckit
 
 #endif
--- src/eckit/filesystem/TmpFile.h
+++ src/eckit/filesystem/TmpFile.h
@@ -1,15 +1,15 @@
 /*
  * (C) Copyright 1996-2017 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
-// File TmpFile.h
-// Baudouin Raoult - ECMWF Sep 01
+/// @author Baudouin Raoult
+/// @date   Sep 2001
 
 #ifndef eckit_TmpFile_h
 #define eckit_TmpFile_h
@@ -17,24 +17,20 @@
 #include "eckit/filesystem/PathName.h"
 #include "eckit/memory/NonCopyable.h"
 
-//-----------------------------------------------------------------------------
 
 namespace eckit {
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
-class TmpFile : public  PathName,
-				private NonCopyable {
+class TmpFile : public PathName, private NonCopyable {
 public:
+    TmpFile();
 
-	TmpFile();
-
-	~TmpFile(); 
-
+    ~TmpFile();
 };
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
-} // namespace eckit
+}  // namespace eckit
 
 #endif

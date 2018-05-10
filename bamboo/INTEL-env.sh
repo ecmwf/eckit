2a9f81d693821016bf5e73ba277304514ee2b092
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-05-10 17:19:14 +0100

    Fix Bamboo Intel compiler cmake requirements

--- bamboo/INTEL-env.sh
+++ bamboo/INTEL-env.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Initialise module environment if it is not
+# initialise module environment if it is not
 if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
   . /usr/local/apps/module/init/bash
 fi
-# Unload modules not available for Intel
+
 module unload grib_api
 module unload eccodes
 module unload emos
 module unload fftw
 module unload libemos
+
 module load cmake/3.10.2
+
 module switch gnu intel/16.0.3

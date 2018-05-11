ff5781eaec1a4256f835148dd1ec752e40f81e34
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-05-11 16:22:34 +0100

    Fix bamboo

--- bamboo/CLANG-env.sh
+++ bamboo/CLANG-env.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# No module environment on the Mac
 [[ $(uname) == "Darwin" ]] && return
-# Initialise module environment if it is not
+
+# initialise module environment if it is not
 if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
   . /usr/local/apps/module/init/bash
 fi
+
 module unload grib_api
-module unload emos
 module unload eccodes
+module unload emos
+module unload fftw
+module unload libemos
+
+module load cmake/3.10.2
+
 module switch gnu clang

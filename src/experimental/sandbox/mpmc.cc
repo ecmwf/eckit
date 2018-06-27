af32b9aa852b54a64fd47c758d8f670129192ee2
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-06-27 19:28:06 +0200

    sandbox

--- src/experimental/sandbox/mpmc.cc
+++ src/experimental/sandbox/mpmc.cc
@@ -1,38 +1,56 @@
-// #include "eckit/container/Queue.h"
+#include <chrono>
+#include <thread>
 
-int main()
-{
-  #if 0
-  Queue<int> q(8);
+#include "eckit/container/Queue.h"
 
+void mlog(const char* s, int e) {
+  static std::mutex mutex;
+  std::unique_lock<std::mutex> locker(mutex);
+  std::cout << s << e << std::endl;
+}
+
+void plog(int e) {
+    mlog(" -> ", e);
+}
+
+void clog(int e) {
+    mlog(" <- ", e);
+}
+
+int main() {
+  size_t nprod = 19;
+  size_t ncons = 23;
+
+  eckit::Queue<int> q(8);
 
   std::vector<std::thread> producers;
-  for (int id = 0 ; id < nProducers ; ++id) {
-    producers.emplace_back(std::thread([&q, id]{
-              for (int j = 0; j < nbToProduce; ++j) {
-                std::ostringstream tmp; tmp << id << " --> " << 10*id+j; print(tmp.str());
-                q.push(10*id+j);
-              }
-        }));
+  for (int id = 0; id < nprod; ++id) {
+    producers.emplace_back(std::thread([&q, id, ncons] {
+      for (int j = 0; j < ncons; ++j) {
+          int e = 1000 * id + j;
+          q.push(e);
+          plog(e);
+          std::this_thread::sleep_for(std::chrono::milliseconds(3));
+      }
+    }));
   }
-
 
   std::vector<std::thread> consumers;
-  for (int id = 0 ; id < nConsumers ; ++id) {
-        consumers.emplace_back(std::thread([&q, id]{
-              for (int j = 0; j < nbToConsume; ++j) {
-                auto e = q.pop();
-                std::ostringstream tmp; tmp << id << "    <-- " << e; print(tmp.str());
-              }
-        }));
+  for (int id = 0; id < ncons; ++id) {
+    consumers.emplace_back(std::thread([&q, id, nprod] {
+      for (int j = 0; j < nprod; ++j) {
+        auto e = q.pop();
+        clog(e);
+        std::this_thread::sleep_for(std::chrono::milliseconds(3));
+      }
+    }));
   }
 
-  for(auto& p : producers) {
+  for (auto& p : producers) {
     p.join();
   }
 
-  for(auto& c : consumers) {
+  for (auto& c : consumers) {
     c.join();
   }
-  #endif
 }

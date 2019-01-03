040f43d6fe21c779213e201836bda354d87d7702
Tiago Quintino <tiago.quintino@ecmwf.int>
2019-01-03 13:55:31 +0000

    cleanup bamboo

--- bamboo/INTEL-env.sh
+++ bamboo/INTEL-env.sh
@@ -14,4 +14,4 @@ module unload metview
 
 module load cmake/3.10.2
 
-module switch gnu intel/16.0.3
+module switch gnu intel/17.0.3
--- bamboo/env.sh
+++ bamboo/env.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 export ECKIT_TEST_THREADS=8
+# ctest_parallel="no"

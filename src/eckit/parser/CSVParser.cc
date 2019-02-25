288176b3b94f8a607d2661824fdb491f8883d5ae
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2019-02-25 22:45:32 +0000

    CSV parser

--- src/eckit/parser/CSVParser.cc
+++ src/eckit/parser/CSVParser.cc
@@ -53,12 +53,12 @@ Value CSVParser::parse()
                 break;
             }
             ValueMap m;
-            if(header.size() != line.size()) {
+            if (header.size() != line.size()) {
                 std::cout << header << std::endl;
-                 std::cout << line << std::endl;
-                 std::cout <<header.size()<< std::endl;
-                 std::cout <<line.size()<< std::endl;
-           }
+                std::cout << line << std::endl;
+                std::cout << header.size() << std::endl;
+                std::cout << line.size() << std::endl;
+            }
             ASSERT(header.size() == line.size());
             for (size_t i = 0; i < header.size(); ++i) {
                 m[header[i]] = line[i];
@@ -83,9 +83,17 @@ Value CSVParser::parse()
 Value CSVParser::nextItem(bool& comma) {
     for (;;) {
         int c = peek(true);
+
+        if (c == 0) {
+            break;
+        }
+
         if (c != '\n' && c != '\r') {
             break;
         }
+
+        next(true);
+
     }
 
     std::string result;
@@ -93,6 +101,7 @@ Value CSVParser::nextItem(bool& comma) {
 
     while (peek(true)) {
         int c = next(true);
+
         if (c == 0 || c == ',' || c == '\n' || c == '\r') {
             comma = (c == ',');
             return result;
@@ -114,7 +123,7 @@ ValueList CSVParser::nextLine() {
 }
 
 const Value& CSVParser::header() const {
-    if(header_.isNil()) {
+    if (header_.isNil()) {
         CSVParser& self = const_cast<CSVParser&>(*this);
         self.header_ = self.nextLine();
     }

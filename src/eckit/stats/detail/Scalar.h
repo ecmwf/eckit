3071049f3b1d409200621effc3fbcc9b9b785596
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2019-03-26 18:03:20 +0000

    MIR-352, MIR-353, MIR-354: field statistics

--- src/eckit/stats/detail/Scalar.h
+++ src/eckit/stats/detail/Scalar.h
@@ -25,47 +25,10 @@ namespace detail {
 
 
 /// Scalar statistics (composition)
-struct Scalar {
-private:
-    PNorms pNorms_;
-    CentralMomentsT<double> centralMoments_;
-
-public:
-
-    void reset() {
-        centralMoments_.reset();
-        pNorms_.reset();
-    }
-
-    double mean()              const { return centralMoments_.mean(); }
-    double variance()          const { return centralMoments_.variance(); }
-    double standardDeviation() const { return centralMoments_.standardDeviation(); }
-    double skewness()          const { return centralMoments_.skewness(); }
-    double kurtosis()          const { return centralMoments_.kurtosis(); }
-
-    double normL1()        const { return pNorms_.normL1(); }
-    double normL2()        const { return pNorms_.normL2(); }
-    double normLinfinity() const { return pNorms_.normLinfinity(); }
-
-    double difference(const double& a, const double& b) const {
-        return std::abs(a - b);
-    }
-
-    void operator()(const double& v) {
-        centralMoments_(v);
-        pNorms_        (v);
-    }
-
-    void operator+=(const Scalar& other) {
-        centralMoments_ += other.centralMoments_;
-        pNorms_         += other.pNorms_;
-    }
-
+struct Scalar : public CentralMomentsT<double> {
     void print(std::ostream& out) const {
         out << "Scalar[";
-        centralMoments_.print(out);
-        out << ",";
-        pNorms_.print(out);
+        CentralMomentsT<double>::print(out);
         out << "]";
     }
 };

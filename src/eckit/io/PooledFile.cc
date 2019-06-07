2a43ab026e722a0568d4597f621943ea741c4771
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2019-06-07 12:12:46 +0100

    Add PooledFile

---
+++ src/eckit/io/PooledFile.cc
@@ -0,0 +1,93 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#include <stdio.h>
+
+#include "eckit/filesystem/PathName.h"
+#include "eckit/exception/Exceptions.h"
+#include "eckit/io/PooledFile.h"
+
+namespace eckit {
+
+
+
+class PoolEntry {
+    std::string name_;
+
+    size_t count_;
+
+    FILE* file_;
+public:
+    PoolEntry(const std: string& name):
+        name_(name),
+        count_(0),
+        file_(0) {
+    }
+
+    void add(PooledFile*) {
+        count_++;
+    }
+
+    void remove(PooledFile*) {
+        count_--;
+    }
+
+    void open() {
+        if(!file_) {
+            file_ = fopen(name_, "r");
+            if (file_ == 0){
+                            throw CantOpenFile(name_, errno == ENOENT);
+            }
+        }
+    }
+
+    void close() {
+        if(count_ == 0) {
+            fclose(file_);
+        }
+    }
+
+}
+
+// Use thread singletin
+std::map<std::string, PoolEntry> pool_;
+
+
+PooledFile::PooledFile(const std::string& name):
+    name_(name)
+{
+    if (pool_.find(name) == pool_.end()) {
+        pool_[name] = PoolEntry(name);
+    }
+    pool_[name].add(this);
+}
+
+PooledFile::~PooledFile() {
+    ASSERT(pool_.find(name) != pool_.end());
+    pool_[name].remove(this);
+}
+
+void PooledFile::open() {
+    ASSERT(pool_.find(name) != pool_.end());
+    return pool_[name_].open(this);
+}
+
+void PooledFile::close() noexcept(false) {
+    ASSERT(pool_.find(name) != pool_.end());
+    return pool_[name_].close(this);
+}
+
+PooledFile::operator FILE*() {
+    ASSERT(pool_.find(name) != pool_.end());
+    return pool_[name_].file();
+}
+
+} // namespace eckit
+
---
+++ src/eckit/io/PooledFile.h
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date   June 96
+
+#ifndef eckit_io_PooledFile_h
+#define eckit_io_PooledFile_h
+
+#include <stdio.h>
+
+#include "eckit/filesystem/PathName.h"
+#include "eckit/memory/NonCopyable.h"
+
+
+namespace eckit {
+
+class PathName;
+
+/// Wrapper around a stdio FILE*
+/// Use this for class members
+
+class PooledFile : private NonCopyable {
+public:
+
+    PooledFile(const PathName& name, const std::string& mode = "r");
+
+    /// @pre must have been closed
+    ~PooledFile();
+
+    /// Get the FILE* but don't call fclose on it
+    operator FILE*() { return file_; }
+
+    bool isOpen() { return file_; }
+
+    /// @throws on fclose failure
+    void close() noexcept(false);
+
+private: // members
+
+	FILE *file_;
+
+};
+
+
+/// Wrapper around a stdio FILE*
+/// Use this for stack objects that automatically close
+
+class AutoPooledFile : public PooledFile {
+public:
+    AutoPooledFile(const PathName& name, const std::string& mode = "r") : PooledFile(name, mode)
+    {}
+    ~AutoPooledFile() noexcept(false)
+    { close(); }
+};
+
+
+} // namespace eckit
+
+#endif

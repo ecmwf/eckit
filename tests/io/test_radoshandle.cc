c4f773c60969b3151f42fab1b7c7aaa7e3bf6a25
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2019-06-26 22:50:06 +0100

    ECKIT-403 some progress on rados

--- tests/io/test_radoshandle.cc
+++ tests/io/test_radoshandle.cc
@@ -30,7 +30,7 @@ CASE("RadosHandle") {
 
     const char buf[] = "abcdefghijklmnopqrstuvwxyz";
 
-    RadosHandle h("foobar");
+    RadosHandle h("fdb:foobar");
     std::cout << "====> " << h << std::endl;
 
     h.openForWrite(sizeof(buf));
@@ -40,7 +40,7 @@ CASE("RadosHandle") {
     std::cout << "write done" << std::endl;
 
     Buffer mem(1024);
-    RadosHandle g("foobar");
+    RadosHandle g("fdb:foobar");
     std::cout << "====> " << g << std::endl;
 
     std::cout << "Size is " << g.openForRead();
@@ -60,7 +60,7 @@ CASE("RadosWriteHandle") {
 
     const char buf[] = "abcdefghijklmnopqrstuvwxyz";
 
-    RadosWriteHandle h("foobar", 2);
+    RadosWriteHandle h("fdb:foobar", 2);
     std::cout << "====> " << h << std::endl;
 
     h.openForWrite(sizeof(buf));

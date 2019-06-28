c23110b2236861b0332dc0848a7ddeef6ebc4499
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2019-06-28 09:52:31 +0100

    ECKIT-403 some progress on rados

--- src/eckit/io/rados/RadosWriteHandle.cc
+++ src/eckit/io/rados/RadosWriteHandle.cc
@@ -87,29 +87,36 @@ long RadosWriteHandle::read(void* buffer, long length) {
 
 long RadosWriteHandle::write(const void* buffer, long length) {
 
-
     std::cout << "RadosWriteHandle::write " << length << std::endl;
-
     ASSERT(opened_);
+
+    if(length == 0) {
+        return 0;
+    }
+
 
     long result = 0;
     const char* buf = reinterpret_cast<const char*>(buffer);
 
     while (length > 0) {
 
-        Length len = std::min(maxObjectSize_, Length(length));
+        Length len = std::min(Length(maxObjectSize_ - Length(written_)), Length(length));
+        long l     = (long)len;
+        ASSERT(len == Length(l));
+
+        if(l == 0) {
+            ASSERT(handle_);
+            handle_->close();
+            handle_.reset(0);
+            written_ = 0;
+            continue;
+        }
 
         std::cout << "RadosWriteHandle::write " << len << " - " << maxObjectSize_ << " - " << written_ << std::endl;
-
-
-        long l     = (long)len;
-
-        ASSERT(len == Length(l));
 
         if (!handle_.get()) {
 
             RadosObject object(object_, part_++);
-
             std::cout << "RadosWriteHandle::write open " << object << std::endl;
             handle_.reset(new RadosHandle(object));
             handle_->openForWrite(0); // TODO: use proper size
@@ -120,13 +127,6 @@ long RadosWriteHandle::write(const void* buffer, long length) {
         written_ += l;
         result += l;
         length -= l;
-
-        ASSERT(Length(written_) <= maxObjectSize_);
-
-        if (Length(written_) == maxObjectSize_) {
-            handle_->close();
-            handle_.reset(0);
-        }
 
     }
 

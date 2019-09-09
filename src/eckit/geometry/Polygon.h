ae5246b7b61f49f80693eb76323198174fbfc1af
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2019-09-09 15:10:45 +0100

    ECKIT-420 added a small class for handling 2D polygons

---
+++ src/eckit/geometry/Polygon.h
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef Polygon_H
+#define Polygon_H
+
+#include <deque>
+#include "eckit/geometry/Point2.h"
+
+//------------------------------------------------------------------------------------------------------
+
+namespace eckit {
+namespace geometry {
+
+//------------------------------------------------------------------------------------------------------
+
+class Polygon
+{
+public:
+    Polygon() {}
+
+    Polygon(std::initializer_list<Point2> vv) {
+        assert(c.size() <= std::size(m_array));
+        for (auto& v : vv)
+            vertices.push_back(v);
+    }
+
+    void push_back(const Point2& p) {
+        vertices.push_back(p);
+    }
+    void push_front(const Point2& p) {
+        vertices.push_front(p);
+    }
+    bool sameAs(const Polygon& p) const {
+        return vertices == p.vertices;
+    }
+    bool congruent(const Polygon& p) const;
+
+
+    void print(std::ostream& s) const;
+
+    friend std::ostream& operator<<(std::ostream& s, const Polygon& p) {
+        p.print(s);
+        return s;
+    }
+
+private: //members
+    std::deque<Point2> vertices;
+};
+
+//------------------------------------------------------------------------------------------------------
+
+} // namespace geometry
+} // namespace eckit
+
+#endif // Polygon_H

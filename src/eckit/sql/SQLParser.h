c60e3cc67b68097c0eff2b4f29b391ad422f2f66
Tiago Quintino <Tiago.Quintino@ecmwf.int>
2019-12-05 11:40:18 +0000

    remove last mention of ecml

--- src/eckit/sql/SQLParser.h
+++ src/eckit/sql/SQLParser.h
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2012 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -59,7 +59,6 @@ private:
 
     static void parseStringInternal(SQLSession&, const std::string&);
 
-    // Migrated from SQLHandler (ecml verb) as part of removal of ecml.
     static std::string cleanUpSQLText(const std::string&);
 };
 

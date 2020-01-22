2d8e37678e944cac1c06be31a043f9c5ebcb04b8
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-01-22 18:52:52 +0000

    Work on pooled handle

--- src/eckit/io/PartFileHandle.cc
+++ src/eckit/io/PartFileHandle.cc
@@ -249,12 +249,6 @@ Length PartFileHandle::estimate() {
     return std::accumulate(length_.begin(), length_.end(), Length(0));
 }
 
-// void PartFileHandle::toRemote(Stream& s) const {
-//     MarsFSPath p(PathName(path_).clusterName());
-//     PartFileHandle remote(p, offset_, length_);
-//     s << remote;
-// }
-
 void PartFileHandle::cost(std::map<std::string, Length>& c, bool read) const {
     if (read) {
         c[NodeInfo::thisNode().node()] += const_cast<PartFileHandle*>(this)->estimate();

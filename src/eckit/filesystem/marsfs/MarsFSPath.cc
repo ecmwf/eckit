86ed359870aa33c51f92f76e5fa0112329a35971
Tiago Quintino <tiago.quintino@ecmwf.int>
2020-02-06 15:19:44 +0000

    Add some prints

--- src/eckit/filesystem/marsfs/MarsFSPath.cc
+++ src/eckit/filesystem/marsfs/MarsFSPath.cc
@@ -21,7 +21,9 @@
 #include "eckit/thread/Mutex.h"
 #include "eckit/utils/MD5.h"
 
-//----------------------------------------------------------------------------------------------------------------------
+#include "eckit/os/BackTrace.h"
+
+
 
 namespace eckit {
 
@@ -87,6 +89,7 @@ void operator<<(Stream& s, MarsFSPath const& path) {
 }
 
 bool MarsFSPath::isLocal() const {
+    Log::info() << "MarsFSPath::isLocal() node_: " << node_ << " node(): " << NodeInfo::thisNode().node() << " backtrace:" << BackTrace::dump() << std::endl;
     return node_ == NodeInfo::thisNode().node();
 }
 
@@ -221,6 +224,7 @@ void MarsFSPath::rename(const MarsFSPath& from, const MarsFSPath& to) {
 }
 
 DataHandle* MarsFSPath::fileHandle(bool overwrite) const {
+    Log::info() << "MarsFSPath::fileHandle() path_: " << path_ << std::endl;
     if(isLocal()) {
         return new FileHandle(path_, overwrite);
     }

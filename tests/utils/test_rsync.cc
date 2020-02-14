ac06dcab73d70196b6b823a3b8660285b1bf2cf6
Tiago Quintino <tiago.quintino@ecmwf.int>
2020-02-14 22:50:40 +0000

    ECKIT-465: more tests

--- tests/utils/test_rsync.cc
+++ tests/utils/test_rsync.cc
@@ -11,8 +11,10 @@
 #include <fstream>
 #include <string>
 
+#include "eckit/filesystem/LocalPathName.h"
 #include "eckit/filesystem/PathName.h"
 #include "eckit/filesystem/TmpFile.h"
+#include "eckit/io/DataHandle.h"
 #include "eckit/utils/Rsync.h"
 
 #include "eckit/testing/Test.h"
@@ -24,36 +26,121 @@ using namespace eckit::testing;
 namespace eckit {
 namespace test {
 
-bool sameContents(PathName file1, PathName file2) {
-    std::ifstream ifs1(file1.localPath());
-    std::ifstream ifs2(file2.localPath());
-
-    for (std::string line1, line2; !ifs1.eof() && !ifs2.eof(); ) {
-        std::getline(ifs1, line1);
-        std::getline(ifs2, line2);
-        if(line1 != line2)
-            return false;
-    }
-
-    return ifs1.eof() && ifs2.eof();
+bool isSame(PathName right, PathName left) {
+    std::unique_ptr<DataHandle> rhs(right.fileHandle());
+    std::unique_ptr<DataHandle> lhs(left.fileHandle());
+    return rhs->compare(*lhs);
 }
 
-CASE("File sync") {
-    TmpFile source, destination;
+CASE("File sync to inexistent file") {
 
+    PathName source = PathName::unique(LocalPathName::cwd());
     {
         std::ofstream ofs(source.localPath());
         ofs << "The quick brown fox jumps over the lazy dog" << std::endl;
     }
+
+    PathName target = PathName::unique(LocalPathName::cwd());
+
+    Rsync rsync;
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
+}
+
+CASE("File sync from empty file ") {
+
+    PathName source = PathName::unique(LocalPathName::cwd());
+    source.touch();
+
+    PathName target = PathName::unique(LocalPathName::cwd());
     {
-        std::ofstream ofs(destination.localPath());
-        ofs << "The quick brown fox jumps over the lazy duck" << std::endl;
+        std::ofstream ofs(source.localPath());
+        ofs << "The quick brown fox jumps over the lazy dog" << std::endl;
     }
 
     Rsync rsync;
-    rsync.syncData(source, destination);
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
+}
 
-    EXPECT(sameContents(source, destination));
+CASE("File sync to existing empty file") {
+
+    PathName source = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(source.localPath());
+        ofs << "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."
+            << std::endl;
+    }
+
+    PathName target = PathName::unique(LocalPathName::cwd());
+    target.touch();
+
+    Rsync rsync;
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
+}
+
+CASE("Delta in the begining") {
+
+    PathName source = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(source.localPath());
+        ofs << "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born "
+               "and I will give you a complete account of the system..."
+            << std::endl;
+    }
+
+    PathName target = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(target.localPath());
+        ofs << "He should explain to you how all this mistaken idea of denouncing pleasure and praising pain was born "
+               "and I will give you a complete account of the system..."
+            << std::endl;
+    }
+
+    Rsync rsync;
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
+}
+
+CASE("Delta in the middle") {
+
+    PathName source = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(source.localPath());
+        ofs << "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into "
+               "a horrible vermin."
+            << std::endl;
+    }
+
+    PathName target = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(target.localPath());
+        ofs << "One morning transformed in his bed into a horrible vermin." << std::endl;
+    }
+
+    Rsync rsync;
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
+}
+
+CASE("Delta at the end") {
+
+    PathName source = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(source.localPath());
+        ofs << "The quick brown fox jumps over the lazy dog" << std::endl;
+    }
+
+    PathName target = PathName::unique(LocalPathName::cwd());
+    {
+        std::ofstream ofs(target.localPath());
+        ofs << "The quick brown fox jumps over the curious duck" << std::endl;
+    }
+
+    Rsync rsync;
+    rsync.syncData(source, target);
+    EXPECT(isSame(source, target));
 }
 
 }  // end namespace test

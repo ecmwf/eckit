4559d05897e6c2fb56cf260edecda793050aef6b
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2022-05-04 15:45:53 +0100

    ECKIT-594 Move eckit::geometry::Polygon to eckit::geometry::polygon::Polygon

--- src/eckit/geometry/polygon/Polygon.h
+++ src/eckit/geometry/polygon/Polygon.h
@@ -8,50 +8,51 @@
  * does it submit to any jurisdiction.
  */
 
-#ifndef Polygon_H
-#define Polygon_H
+#pragma once
 
 #include <deque>
+#include <iosfwd>
+
 #include "eckit/geometry/Point2.h"
 
 //------------------------------------------------------------------------------------------------------
 
 namespace eckit {
 namespace geometry {
+namespace polygon {
 
 //------------------------------------------------------------------------------------------------------
 
-class Polygon {
+class Polygon : protected std::deque<Point2> {
+    using container_type = std::deque<value_type>;
+
 public:
-    Polygon() {}
+    Polygon() = default;
 
-    Polygon(std::initializer_list<Point2> vv) {
-        for (auto& v : vv)
-            vertices.push_back(v);
-    }
+    Polygon(std::initializer_list<value_type> l) :
+        container_type(l) {}
 
-    void push_back(const Point2& p) { vertices.push_back(p); }
-    void push_front(const Point2& p) { vertices.push_front(p); }
-    size_t num_vertices() const { return vertices.size(); }
-    const Point2& vertex(size_t idx) const { return vertices.at(idx); }
-    bool sameAs(const Polygon& p) const { return vertices == p.vertices; }
-    bool congruent(const Polygon& p) const;
+    using container_type::push_back;
+    using container_type::push_front;
 
+    size_t num_vertices() const { return size(); }
 
-    void print(std::ostream& s) const;
+    const value_type& vertex(size_t idx) const { return at(idx); }
+
+    bool sameAs(const Polygon& p) const { return *this == p; }
+
+    bool congruent(const Polygon&) const;
+
+    void print(std::ostream&) const;
 
     friend std::ostream& operator<<(std::ostream& s, const Polygon& p) {
         p.print(s);
         return s;
     }
-
-private:  // members
-    std::deque<Point2> vertices;
 };
 
 //------------------------------------------------------------------------------------------------------
 
+}  // namespace polygon
 }  // namespace geometry
 }  // namespace eckit
-
-#endif  // Polygon_H
--- tests/geometry/test_polygon.cc
+++ tests/geometry/test_polygon.cc
@@ -8,86 +8,87 @@
  * does it submit to any jurisdiction.
  */
 
-#include "eckit/geometry/Polygon.h"
+#include "eckit/geometry/Point2.h"
+#include "eckit/geometry/polygon/Polygon.h"
 #include "eckit/testing/Test.h"
-
-using namespace std;
-using namespace eckit;
-using namespace eckit::testing;
-using namespace eckit::geometry;
 
 namespace eckit {
 namespace test {
 
-CASE("empty polygon") {
-    Polygon poly1;
-    Polygon poly2;
+CASE("Polygon") {
+    using eckit::geometry::Point2;
+    using eckit::geometry::polygon::Polygon;
 
-    EXPECT(poly1.sameAs(poly2));
-}
+    SECTION("empty polygon") {
+        Polygon poly1;
+        Polygon poly2;
 
-CASE("equality") {
-    Polygon poly1;
-    Polygon poly2;
+        EXPECT(poly1.sameAs(poly2));
+    }
 
-    EXPECT(poly1.num_vertices() == 0);
+    SECTION("equality") {
+        Polygon poly1;
+        Polygon poly2;
 
-    Point2 p1 = {1.0, 2.0};
-    poly1.push_front(p1);
-    EXPECT(poly1.num_vertices() == 1);
-    EXPECT(poly1.vertex(0) == p1);
+        EXPECT(poly1.num_vertices() == 0);
 
-    EXPECT(!poly1.sameAs(poly2));
+        Point2 p1 = {1.0, 2.0};
+        poly1.push_front(p1);
+        EXPECT(poly1.num_vertices() == 1);
+        EXPECT(poly1.vertex(0) == p1);
 
-    poly2.push_back(p1);
-    EXPECT(poly1.sameAs(poly2));
+        EXPECT(!poly1.sameAs(poly2));
 
-    Point2 p2 = {2.0, 1.0};
-    poly1.push_front(p2);
-    EXPECT(!poly1.sameAs(poly2));
+        poly2.push_back(p1);
+        EXPECT(poly1.sameAs(poly2));
 
-    poly2.push_back(p2);
-    EXPECT(!poly1.sameAs(poly2));
-}
+        Point2 p2 = {2.0, 1.0};
+        poly1.push_front(p2);
+        EXPECT(!poly1.sameAs(poly2));
 
-CASE("congruence") {
-    Polygon poly1;
-    Polygon poly2;
-    EXPECT(poly1.congruent(poly2));
+        poly2.push_back(p2);
+        EXPECT(!poly1.sameAs(poly2));
+    }
 
-    Point2 p1 = {1.0, 2.0};
-    poly1.push_front(p1);
-    EXPECT(!poly1.congruent(poly2));
+    SECTION("congruence") {
+        Polygon poly1;
+        Polygon poly2;
+        EXPECT(poly1.congruent(poly2));
 
-    poly2.push_back(p1);
-    EXPECT(poly1.congruent(poly2));
+        Point2 p1 = {1.0, 2.0};
+        poly1.push_front(p1);
+        EXPECT(!poly1.congruent(poly2));
 
-    Point2 p2 = {2.0, 1.0};
-    poly1.push_front(p2);
-    EXPECT(!poly1.congruent(poly2));
+        poly2.push_back(p1);
+        EXPECT(poly1.congruent(poly2));
 
-    poly2.push_back(p2);
-    EXPECT(poly1.congruent(poly2));
+        Point2 p2 = {2.0, 1.0};
+        poly1.push_front(p2);
+        EXPECT(!poly1.congruent(poly2));
 
-    Point2 p3 = {3.0, 4.0};
-    poly2.push_back(p3);
-    Polygon poly3 = {p2, p3, p1};
-    EXPECT(!poly2.sameAs(poly3));
-    EXPECT(poly2.congruent(poly3));
+        poly2.push_back(p2);
+        EXPECT(poly1.congruent(poly2));
 
-    EXPECT(poly2.num_vertices() == 3);
-    EXPECT(poly2.vertex(2) == poly3.vertex(1));
+        Point2 p3 = {3.0, 4.0};
+        poly2.push_back(p3);
+        Polygon poly3 = {p2, p3, p1};
+        EXPECT(!poly2.sameAs(poly3));
+        EXPECT(poly2.congruent(poly3));
 
-    Polygon poly4 = {p3, p1, p2};
-    EXPECT(!poly2.sameAs(poly4));
-    EXPECT(poly2.congruent(poly4));
+        EXPECT(poly2.num_vertices() == 3);
+        EXPECT(poly2.vertex(2) == poly3.vertex(1));
+
+        Polygon poly4 = {p3, p1, p2};
+        EXPECT(!poly2.sameAs(poly4));
+        EXPECT(poly2.congruent(poly4));
+    }
 }
 
 }  // namespace test
 }  // namespace eckit
 
-//----------------------------------------------------------------------------------------------------------------------
-
 int main(int argc, char** argv) {
+    using namespace eckit::testing;
+
     return run_tests(argc, argv);
 }

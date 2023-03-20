835c171cb4ccc4d37418d900d50514e05814c357
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-03-20 11:52:07 +0000

    util

---
+++ src/griditer/util/arange.cc
@@ -0,0 +1,29 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#pragma once
+
+#include <cassert>
+
+#include "griditer/util.h"
+
+
+namespace grit::util {
+
+
+std::vector<double> arange(double start, double stop, double step) {
+    return {};
+}
+
+
+}
+

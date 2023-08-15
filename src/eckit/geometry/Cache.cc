40177a13bd27ed42d9df43c24bc220818b0bda88
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-08-15 12:02:25 +0100

    eckit::geometry

---
+++ src/eckit/geometry/Cache.cc
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/geometry/Cache.h"
+
+
+#include <map>
+#include <memory>
+#include <utility>
+
+
+namespace eckit::geometry::cache {
+
+
+static std::map<std::string, std::unique_ptr<Cache>> __cache;
+
+
+class ClassA {
+public:
+    void functionA() const {}
+};
+
+
+class ClassB {
+public:
+    void functionB() const {}
+};
+
+
+void test() {
+    __cache.insert({"a", std::make_unique<CacheableT<ClassA>>(ClassA())});
+    __cache.insert({"b", std::make_unique<CacheableT<ClassB>>(ClassB())});
+
+    __cache["b"]->call();
+}
+
+
+}  // namespace eckit::geometry::cache
---
+++ src/eckit/geometry/Cache.h
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include <string>
+#include <type_traits>
+
+
+namespace eckit::geometry::cache {
+
+
+class Cache {
+public:
+    virtual ~Cache()    = default;
+    virtual void call() = 0;
+};
+
+
+class ClassA;
+class ClassB;
+
+
+template <typename T>
+class CacheableT : public Cache {
+    T obj;
+
+public:
+    CacheableT(T&& o) :
+        obj(std::move(o)) {}
+
+    void call() override {
+        if constexpr (std::is_same<T, ClassA>::value) {
+            obj.functionA();
+            return;
+        }
+
+        if constexpr (std::is_same<T, ClassB>::value) {
+            obj.functionB();
+            return;
+        }
+
+        return;
+    }
+};
+
+
+}  // namespace eckit::geometry::cache

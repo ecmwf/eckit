53bc06446496b9f71f09262681f5980f469afb4b
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-08-16 23:55:43 +0100

    eckit::geometry, eckit::cmd

---
+++ src/eckit/geometry/LibEcKitGeometry.cc
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/geometry/LibEcKitGeometry.h"
+
+#include "eckit/eckit_version.h"
+
+
+namespace eckit {
+
+
+REGISTER_LIBRARY(LibEcKitGeometry);
+
+
+LibEcKitGeometry::LibEcKitGeometry() :
+    Library("eckit_geometry") {}
+
+
+LibEcKitGeometry& LibEcKitGeometry::instance() {
+    static LibEcKitGeometry lib;
+    return lib;
+}
+
+
+const void* LibEcKitGeometry::addr() const {
+    return this;
+}
+
+
+std::string LibEcKitGeometry::version() const {
+    return eckit_version_str();
+}
+
+
+std::string LibEcKitGeometry::gitsha1(unsigned int count) const {
+    std::string sha1(eckit_git_sha1());
+    return sha1.empty() ? "not available" : sha1.substr(0, std::min(count, 40U));
+}
+
+
+}  // namespace eckit

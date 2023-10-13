aebe1bb1a78278bee7493896e921427cf29b3f38
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-10-13 11:35:16 +0000

    Fixes to MeshBuilder validate_mesh_vs_grid
    - comparison function was multiplying instead of adding 1.e-16
    - scoped index i was shadowing loop index i

---
+++ src/atlas/array.h
@@ -0,0 +1,30 @@
+/*
+ * (C) Copyright 2013 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/// @file array.h
+/// @brief Public API header that includes all relevant headers for using the atlas::array namespace
+/// @author Willem Deconinck
+
+/// @namespace atlas::array
+/// @brief Contains classes to operate with multidimensional arrays
+
+#pragma once
+
+#include "atlas/array/Array.h"
+#include "atlas/array/ArrayDataStore.h"
+#include "atlas/array/ArrayShape.h"
+#include "atlas/array/ArraySpec.h"
+#include "atlas/array/ArrayStrides.h"
+#include "atlas/array/ArrayView.h"
+#include "atlas/array/DataType.h"
+#include "atlas/array/LocalView.h"
+#include "atlas/array/MakeView.h"
+#include "atlas/array/Table.h"
+//#include "atlas/array/TableView.h"
---
+++ src/atlas/grid.h
@@ -0,0 +1,22 @@
+/*
+ * (C) Copyright 2013 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/// @author Willem Deconinck
+
+#pragma once
+
+#include "atlas/grid/CubedSphereGrid.h"
+#include "atlas/grid/Distribution.h"
+#include "atlas/grid/Grid.h"
+#include "atlas/grid/Iterator.h"
+#include "atlas/grid/Partitioner.h"
+#include "atlas/grid/SpecRegistry.h"
+#include "atlas/grid/StructuredGrid.h"
+#include "atlas/grid/UnstructuredGrid.h"

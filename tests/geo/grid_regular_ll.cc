d0a67609cf1ee94483faefa447fcb2a6d47b883f
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-11-02 17:01:31 +0000

    eckit::geo RegularLL

---
+++ tests/geo/grid_regular_ll.cc
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include <memory>
+
+#include "eckit/config/MappedConfiguration.h"
+#include "eckit/geo/grid/regular/RegularLL.h"
+#include "eckit/testing/Test.h"
+
+
+namespace eckit::test {
+
+
+using namespace eckit::geo;
+
+
+CASE("") {
+    std::unique_ptr<Grid> global(
+        new grid::regular::RegularLL(MappedConfiguration{{{"grid", std::vector<double>{1, 1}}}}));
+
+    size_t global_size = global->size();
+    EXPECT_EQUAL(global_size, 360 * 181);
+
+    std::unique_ptr<Grid> local(new grid::regular::RegularLL(
+        MappedConfiguration{{{"grid", std::vector<double>{2, 1}}, {"area", std::vector<double>{10, 1, 1, 10}}}}));
+
+    size_t local_size = local->size();
+    EXPECT_EQUAL(local_size, 5 * 10);
+}
+
+
+}  // namespace eckit::test
+
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}

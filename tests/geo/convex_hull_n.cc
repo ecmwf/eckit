c3e8f40f3c3265637fda2419d441555190c23823
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-12-08 17:02:32 +0000

    eckit::geo triangulation/convex hull test

--- tests/geo/convex_hull_n.cc
+++ tests/geo/convex_hull_n.cc
@@ -24,13 +24,10 @@
 namespace eckit::test {
 
 
-using namespace geo;
+CASE("ConvexHullN, N=2") {
+    using Point2 = std::array<double, 2>;
 
-using Point2 = std::array<double, 2>;
-
-
-CASE("PointLonLat normalisation") {
-    std::vector<Point2> points{{
+    geo::convexhull::ConvexHullN ch(std::vector<Point2>{
         {1, 2},  // part of convex hull
         {3, 1},  //...
         {4, 4},
@@ -41,16 +38,42 @@ CASE("PointLonLat normalisation") {
         {8, 3},  //...
         {6, 9},  //...
         {9, 6},  //...
-    }};
+    });
 
-    std::vector<size_t> vertices_reference{0, 1, 4, 5, 7, 8, 9};
 
-    convexhull::ConvexHullN ch(points);
-    auto vertices = ch.list_vertices();
+    SECTION("vertices") {
+        const auto vertices = ch.list_vertices();
+        EXPECT(vertices.size() == 7);
 
-    EXPECT(vertices.size() == vertices_reference.size());
-    for (const auto& vertex : vertices_reference) {
-        EXPECT(std::find(vertices.begin(), vertices.end(), vertex) != vertices.end());
+        for (size_t vertex : {0, 1, 4, 5, 7, 8, 9}) {
+            EXPECT(std::find(vertices.begin(), vertices.end(), vertex) != vertices.end());
+        }
+
+        for (size_t vertex : {2, 3, 6}) {
+            EXPECT(std::find(vertices.begin(), vertices.end(), vertex) == vertices.end());
+        }
+    }
+
+
+    SECTION("facets") {
+        const auto facets = ch.list_facets();
+        EXPECT(facets.size() == 7);
+
+        for (const auto& fr : std::vector<std::vector<size_t>>{
+                 {3, 2},
+                 {5, 1},
+                 {5, 3},
+                 {6, 1},
+                 {6, 4},
+                 {7, 2},
+                 {7, 4},
+             }) {
+            ASSERT(fr.size() == 2);
+            EXPECT(std::count_if(facets.begin(), facets.end(), [&fr](const auto& facet) {
+                       ASSERT(facet.size() == 2);
+                       return (fr[0] == facet[0] && fr[1] == facet[1]) || (fr[0] == facet[1] && fr[1] == facet[0]);
+                   }) == 1);
+        }
     }
 }
 

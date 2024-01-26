1586c23a7292723c47ec1420c2357e8f762318e8
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2024-01-26 14:32:05 +0000

    eckit::geo::Projection

--- tests/geo/projection.cc
+++ tests/geo/projection.cc
@@ -28,93 +28,97 @@ namespace eckit::test {
 
 using namespace geo;
 
+using P = std::unique_ptr<Projection>;
 
-CASE("projection") {
-    using P = std::unique_ptr<Projection>;
+
+CASE("projection: none") {
+    Point p = PointLonLat{1, 1};
+    P projection(ProjectionFactory::instance().get("none").create(spec::Custom{}));
+
+    EXPECT(points_equal(p, projection->inv(p)));
+    EXPECT(points_equal(p, projection->fwd(p)));
+}
+
+
+CASE("projection: rotation") {
+    spec::Custom spec({
+        {"projection", "rotation"},
+        {"south_pole_lat", -91.},
+        {"south_pole_lon", -361.},
+    });
 
     Point p = PointLonLat{1, 1};
+    P projection(ProjectionFactory::instance().get(spec.get_string("projection")).create(spec));
+
+    EXPECT(points_equal(p, projection->inv(projection->fwd(p))));
+    EXPECT(points_equal(p, projection->fwd(projection->inv(p))));
+}
 
 
-    SECTION("projection type: none") {
-        P projection(ProjectionFactory::instance().get("none").create(spec::Custom{}));
-        EXPECT(points_equal(p, projection->inv(p)));
-        EXPECT(points_equal(p, projection->fwd(p)));
+CASE("projection: ll_to_xyz") {
+    Point p = PointLonLat{1, 1};
+    P s1(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"R", 1.}})));
+    P s2(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"a", 1.}, {"b", 1.}})));
+    P s3(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"a", 1.}, {"b", 0.5}})));
+
+    EXPECT(points_equal(p, s1->inv(s1->fwd(p))));
+    EXPECT(points_equal(p, s2->inv(s2->fwd(p))));
+    EXPECT(points_equal(s1->fwd(p), s2->fwd(p)));
+
+    Point q = PointLonLat{1, 0};
+
+    EXPECT(points_equal(s1->fwd(q), s3->fwd(q)));
+    EXPECT(points_equal(s2->fwd(q), s3->fwd(q)));
+
+    struct {
+        PointLonLat a;
+        Point3 b;
+    } tests[] = {
+        {{0, 0}, {1, 0, 0}},
+        {{90, 0}, {0, 1, 0}},
+        {{180, 0}, {-1, 0, 0}},
+        {{270, 0}, {0, -1, 0}},
+        {{0, -90}, {0, 0, -0.5}},
+        {{42, -90}, {0, 0, -0.5}},
+        {{0, 90}, {0, 0, 0.5}},
+        {{42, 90}, {0, 0, 0.5}},
+    };
+
+    for (const auto& test : tests) {
+        EXPECT(points_equal(s3->fwd(test.a), test.b));
+    }
+}
+
+
+CASE("projection: ll_to_xyz") {
+    const PointLonLat p(723., 1.);  // <- FIXME
+
+    projection::LonLatToXYZ to_xyz_r(1.);
+
+    auto q = to_xyz_r.fwd(p);
+    auto r = to_xyz_r.inv(q);
+    std::cout << "p(lat, lon): " << p << " -> p(x,y,z): " << q << " -> p(lat, lon): " << r << std::endl;
+
+    EXPECT(points_equal(p, r));
+
+    // oblate spheroid (supported)
+    projection::LonLatToXYZ to_xyz_ab(3., 2.);
+
+    for (const auto& lon : {0., 90., 180., 270.}) {
+        PointLonLat p{lon, 0.};
+        std::cout << "p(lat, lon): " << p << " -> p_ab(x,y,z): " << to_xyz_ab.fwd(p) << std::endl;
     }
 
-
-    SECTION("projection type: rotation") {
-        spec::Custom spec({
-            {"projection", "rotation"},
-            {"south_pole_lat", -91.},
-            {"south_pole_lon", -361.},
-        });
-
-        P projection(ProjectionFactory::instance().get(spec.get_string("projection")).create(spec));
-
-        EXPECT(points_equal(p, projection->inv(projection->fwd(p))));
-        EXPECT(points_equal(p, projection->fwd(projection->inv(p))));
-    }
+    // problate spheroid (not supported)
+    EXPECT_THROWS(projection::LonLatToXYZ(2., 3.));
+}
 
 
-    SECTION("projection type: ll_to_xyz") {
-        P s1(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"R", 1.}})));
-        P s2(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"a", 1.}, {"b", 1.}})));
-        P s3(ProjectionFactory::instance().get("ll_to_xyz").create(spec::Custom({{"a", 1.}, {"b", 0.5}})));
-
-        EXPECT(points_equal(p, s1->inv(s1->fwd(p))));
-        EXPECT(points_equal(p, s2->inv(s2->fwd(p))));
-        EXPECT(points_equal(s1->fwd(p), s2->fwd(p)));
-
-        Point q = PointLonLat{1, 0};
-
-        EXPECT(points_equal(s1->fwd(q), s3->fwd(q)));
-        EXPECT(points_equal(s2->fwd(q), s3->fwd(q)));
-
-        struct {
-            PointLonLat a;
-            Point3 b;
-        } tests[] = {
-            {{0, 0}, {1, 0, 0}},
-            {{90, 0}, {0, 1, 0}},
-            {{180, 0}, {-1, 0, 0}},
-            {{270, 0}, {0, -1, 0}},
-            {{0, -90}, {0, 0, -0.5}},
-            {{42, -90}, {0, 0, -0.5}},
-            {{0, 90}, {0, 0, 0.5}},
-            {{42, 90}, {0, 0, 0.5}},
-        };
-
-        for (const auto& test : tests) {
-            EXPECT(points_equal(s3->fwd(test.a), test.b));
-        }
-    }
+CASE("projection: rotation") {
+    constexpr double eps = 1e-6;
 
 
-    SECTION("projection type: ll_to_xyz") {
-        const PointLonLat p(723., 1.);  // <- FIXME
-
-        projection::LonLatToXYZ to_xyz_r(1.);
-
-        auto q = to_xyz_r.fwd(p);
-        auto r = to_xyz_r.inv(q);
-        std::cout << "p(lat, lon): " << p << " -> p(x,y,z): " << q << " -> p(lat, lon): " << r << std::endl;
-
-        EXPECT(points_equal(p, r));
-
-        // oblate spheroid (supported)
-        projection::LonLatToXYZ to_xyz_ab(3., 2.);
-
-        for (const auto& lon : {0., 90., 180., 270.}) {
-            PointLonLat p{lon, 0.};
-            std::cout << "p(lat, lon): " << p << " -> p_ab(x,y,z): " << to_xyz_ab.fwd(p) << std::endl;
-        }
-
-        // problate spheroid (not supported)
-        EXPECT_THROWS(projection::LonLatToXYZ(2., 3.));
-    }
-
-
-    SECTION("projection type: rotation") {
+    SECTION("rotation (1)") {
         const PointLonLat p(1, 1);
         int delta[] = {-360, -180, -1, 0, 1, 90, 91, 180};
 
@@ -126,22 +130,21 @@ CASE("projection") {
                                              static_cast<double>(c));
                     EXPECT(rot.rotated() == (a % 360 != 0 || (b - c) % 360 != 0));
 
-                    EXPECT(points_equal(p, rot.inv(rot.fwd(p))));
-                    EXPECT(points_equal(p, rot.fwd(rot.inv(p))));
+                    EXPECT(points_equal(p, rot.inv(rot.fwd(p)), eps));
+                    EXPECT(points_equal(p, rot.fwd(rot.inv(p)), eps));
                 }
             }
         }
     }
 
 
-#if 0
-    SECTION("projection type: rotation") {
+    SECTION("rotation (2)") {
         const int Ni = 12;
         const int Nj = 3;
 
         projection::Rotation rot(182., -46.7, 0.);
         const PointLonLat ref[]{
-            {-178., -46.7},
+            {-178., -46.7},  //
             {-178., -16.7},
             {-178., 13.3},
             {-178., 43.3},
@@ -149,39 +152,39 @@ CASE("projection") {
             {2., 76.7},
             {2., 46.7},
             {-178., -46.7},
-            {-162.62343, -19.46929},
-            {-152.02366, 8.65459},
-            {-139.57464, 36.43683},
-            {-113.10894, 61.43199},
-            {-39.88245, 68.00825},
+            {-162.623427, -19.469294},
+            {-152.023657, 8.654593},
+            {-139.574639, 36.436827},
+            {-113.108943, 61.431994},
+            {-39.882454, 68.008245},
             {2., 46.7},
             {-178., -46.7},
-            {-148.83443, -27.31067},
-            {-129.26346, -3.837},
-            {-110.79116, 20.05422},
-            {-85.87917, 41.36507},
-            {-44.42496, 53.29508},
+            {-148.834426, -27.310675},
+            {-129.263456, -3.837005},
+            {-110.791162, 20.054216},
+            {-85.879167, 41.365070},
+            {-44.424956, 53.295076},
             {2., 46.7},
             {-178., -46.7},
-            {-137.90794, -39.07002},
-            {-109.60146, -21.33906},
+            {-137.907940, -39.070023},
+            {-109.601456, -21.339065},
             {-88., 0.},
-            {-66.39854, 21.33906},
-            {-38.09206, 39.07002},
+            {-66.398544, 21.339065},
+            {-38.092060, 39.070023},
             {2., 46.7},
             {-178., -46.7},
-            {-131.57504, -53.29508},
-            {-90.12083, -41.36507},
-            {-65.20884, -20.05422},
-            {-46.73654, 3.837},
-            {-27.16557, 27.31067},
+            {-131.575044, -53.295076},
+            {-90.120833, -41.365070},
+            {-65.208838, -20.054216},
+            {-46.736544, 3.837005},
+            {-27.165574, 27.310675},
             {2., 46.7},
             {-178., -46.7},
-            {-136.11755, -68.00825},
-            {-62.89106, -61.43199},
-            {-36.42536, -36.43683},
-            {-23.97634, -8.65459},
-            {-13.37657, 19.46929},
+            {-136.117546, -68.008245},
+            {-62.891057, -61.431994},
+            {-36.425361, -36.436827},
+            {-23.976343, -8.654593},
+            {-13.376573, 19.469294},
             {2., 46.7},
             {-178., -46.7},
             {-178., -76.7},
@@ -191,39 +194,39 @@ CASE("projection") {
             {2., 16.7},
             {2., 46.7},
             {-178., -46.7},
-            {140.11755, -68.00825},
-            {66.89106, -61.43199},
-            {40.42536, -36.43683},
-            {27.97634, -8.65459},
-            {17.37657, 19.46929},
+            {140.117546, -68.008245},
+            {66.891057, -61.431994},
+            {40.425361, -36.436827},
+            {27.976343, -8.654593},
+            {17.376573, 19.469294},
             {2., 46.7},
             {-178., -46.7},
-            {135.57504, -53.29508},
-            {94.12083, -41.36507},
-            {69.20884, -20.05422},
-            {50.73654, 3.837},
-            {31.16557, 27.31067},
+            {135.575044, -53.295076},
+            {94.120833, -41.365070},
+            {69.208838, -20.054216},
+            {50.736544, 3.837005},
+            {31.165574, 27.310675},
             {2., 46.7},
             {-178., -46.7},
-            {141.90794, -39.07002},
-            {113.60146, -21.33906},
+            {141.907940, -39.070023},
+            {113.601456, -21.339065},
             {92., 0.},
-            {70.39854, 21.33906},
-            {42.09206, 39.07002},
+            {70.398544, 21.339065},
+            {42.092060, 39.070023},
             {2., 46.7},
             {-178., -46.7},
-            {152.83443, -27.31067},
-            {133.26346, -3.837},
-            {114.79116, 20.05422},
-            {89.87917, 41.36507},
-            {48.42496, 53.29508},
+            {152.834426, -27.310675},
+            {133.263456, -3.837005},
+            {114.791162, 20.054216},
+            {89.879167, 41.365070},
+            {48.424956, 53.295076},
             {2., 46.7},
             {-178., -46.7},
-            {166.62343, -19.46929},
-            {156.02366, 8.65459},
-            {143.57464, 36.43683},
-            {117.10894, 61.43199},
-            {43.88245, 68.00825},
+            {166.623427, -19.469294},
+            {156.023657, 8.654593},
+            {143.574639, 36.436827},
+            {117.108943, 61.431994},
+            {43.882454, 68.008245},
             {2., 46.7},
         };
 
@@ -234,15 +237,14 @@ CASE("projection") {
                 auto b = rot.fwd(a);
                 auto c = rot.inv(b);
 
-                EXPECT(points_equal(b, ref[k]));
-                EXPECT(points_equal(a, c));
+                EXPECT(points_equal(b, ref[k], eps));
+                EXPECT(points_equal(a, c, eps));
             }
         }
     }
-#endif
 
 
-    SECTION("projection type: rotation") {
+    SECTION("rotation (3)") {
         const projection::Rotation non_rotated(0., -90., 0.);
         const projection::Rotation rotation_angle(0., -90., -180.);
         const projection::Rotation rotation_matrix(4., -40., 180.);
@@ -274,88 +276,87 @@ CASE("projection") {
 
         for (const auto& test : tests) {
             auto b = test.rotation.fwd(test.a);
-            EXPECT(points_equal(b, test.b));
+            EXPECT(points_equal(b, test.b, eps));
 
             auto a = test.rotation.inv(b);
-            EXPECT(points_equal(a, test.a));
+            EXPECT(points_equal(a, test.a, eps));
         }
     }
+}
 
 
-    SECTION("projection type: proj") {
-        if (ProjectionFactory::instance().exists("proj")) {
-            std::cout.precision(14);
+CASE("projection: proj") {
+    constexpr double eps = 1e-6;
 
-            PointLonLat a{12., 55.};
 
-            struct {
-                const Point b;
-                const std::string target;
-            } tests[] = {
-                {Point2{691875.632137542, 6098907.825129169}, "+proj=utm +zone=32 +datum=WGS84"},
-                {Point2{691875.632137542, 6098907.825129169}, "EPSG:32632"},
-                {a, "EPSG:4326"},
-                {a, "EPSG:4979"},
-                {Point3{3586469.6567764, 762327.65877826, 5201383.5232023}, "EPSG:4978"},
-                {Point3{3574529.7050235, 759789.74368715, 5219005.2599833}, "+proj=cart +R=6371229."},
-                {Point3{3574399.5431832, 759762.07693392, 5218815.216709}, "+proj=cart +ellps=sphere"},
-                {a, "+proj=latlon +ellps=sphere"},
-            };
+    if (ProjectionFactory::instance().exists("proj")) {
+        std::cout.precision(14);
 
-            for (const auto& test : tests) {
-                P projection(ProjectionFactory::instance().get("proj").create(
-                    spec::Custom{{{"source", "EPSG:4326"}, {"target", test.target}}}));
+        PointLonLat a{12., 55.};
+
+        struct {
+            const Point b;
+            const std::string target;
+        } tests[] = {
+            {Point2{691875.632137542, 6098907.825129169}, "+proj=utm +zone=32 +datum=WGS84"},
+            {Point2{691875.632137542, 6098907.825129169}, "EPSG:32632"},
+            {a, "EPSG:4326"},
+            {a, "EPSG:4979"},
+            {Point3{3586469.6567764, 762327.65877826, 5201383.5232023}, "EPSG:4978"},
+            {Point3{3574529.7050235, 759789.74368715, 5219005.2599833}, "+proj=cart +R=6371229."},
+            {Point3{3574399.5431832, 759762.07693392, 5218815.216709}, "+proj=cart +ellps=sphere"},
+            {a, "+proj=latlon +ellps=sphere"},
+        };
+
+        for (const auto& test : tests) {
+            P projection(ProjectionFactory::instance().get("proj").create(
+                spec::Custom{{{"source", "EPSG:4326"}, {"target", test.target}}}));
 
 #if 0
-        std::cout << "ellipsoid: '" << PROJ::ellipsoid(projection.target())
-                  << std::endl;
+    std::cout << "ellipsoid: '" << PROJ::ellipsoid(projection.target())
+              << std::endl;
 #endif
 
-                auto b = projection->fwd(a);
-                auto c = projection->inv(b);
+            auto b = projection->fwd(a);
+            auto c = projection->inv(b);
 
-                std::cout << "-> a:" << a << " -> fwd(a):" << b << " -> inv(fwd(a)):" << c << std::endl;
+            EXPECT(points_equal(b, test.b, eps));
+            EXPECT(points_equal(c, a, eps));
 
-                EXPECT(points_equal(b, test.b));
-                EXPECT(points_equal(c, a));
+            P reverse(ProjectionFactory::instance().get("proj").create(
+                spec::Custom({{"source", test.target}, {"target", "EPSG:4326"}})));
 
-                P reverse(ProjectionFactory::instance().get("proj").create(
-                    spec::Custom({{"source", test.target}, {"target", "EPSG:4326"}})));
+            auto d = reverse->fwd(test.b);
+            auto e = reverse->inv(d);
 
-                auto d = reverse->fwd(test.b);
-                auto e = reverse->inv(d);
-
-                std::cout << "-> b:" << test.b << " -> fwd(b):" << d << " -> inv(fwd(b)):" << e << std::endl;
-
-                EXPECT(points_equal(d, a));
-                EXPECT(points_equal(e, test.b));
-            }
+            EXPECT(points_equal(d, a, eps));
+            EXPECT(points_equal(e, test.b, eps));
         }
     }
+}
 
 
-    SECTION("projection type: mercator") {
-        PointLonLat first{262.036, 14.7365};
+CASE("projection: mercator") {
+    PointLonLat first{262.036, 14.7365};
 
-        {
-            projection::Mercator projection(0., 14., new figure::Sphere(6371229.), first);
+    {
+        projection::Mercator projection(0., 14., new figure::Sphere(6371229.), first);
 
-            auto p = projection.inv({0, 0});
-            Log::info() << p << std::endl;
+        Point2 a{0., 0.};
+        auto b = projection.inv(a);
+        auto c = projection.fwd(b);
 
-            auto q = projection.fwd(p);
-            Log::info() << q << std::endl;
-        }
+        EXPECT(points_equal(c, a));
+    }
 
-        {
-            projection::Mercator projection(-180., 0., new figure::Sphere(1.), {0, 0});
+    {
+        projection::Mercator projection(-180., 0., new figure::Sphere(1.), {0, 0});
 
-            auto q = projection.fwd({-75, 35});
-            Log::info() << q << std::endl;
+        PointLonLat a{-75., 35.};
+        auto b = projection.fwd({-75, 35});
+        auto c = projection.inv(b);
 
-            auto p = projection.inv(q);
-            Log::info() << p << std::endl;
-        }
+        EXPECT(points_equal(c, a));
     }
 }
 

834e3839aa04fdba2650facdb925c9b6b8b16f21
Metin Cakircali <metin.cakircali@ecmwf.int>
2024-05-28 18:29:56 +0200

    feat(FAM): add FamObject

---
+++ src/eckit/io/fam/FamObject.cc
@@ -0,0 +1,151 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*
+ * This software was developed as part of the Horizon Europe programme funded project OpenCUBE
+ * (Grant agreement: 101092984) horizon-opencube.eu
+ */
+
+#include "eckit/io/fam/FamObject.h"
+
+#include "detail/FamObjectDetail.h"
+
+#include <memory>
+#include <string>
+
+namespace eckit {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+FamObject::FamObject(std::unique_ptr<FamObjectDetail> object) noexcept: impl_ {std::move(object)} { }
+
+FamObject::~FamObject() = default;
+
+bool FamObject::operator==(const FamObject& other) const {
+    const auto desc  = impl_->object()->get_global_descriptor();
+    const auto oDesc = other.impl_->object()->get_global_descriptor();
+    return (desc.regionId == oDesc.regionId && desc.offset == oDesc.offset);
+}
+
+void FamObject::replace(const FamDescriptor& object) {
+    /// @todo unnecessary conversion here
+    impl_->replace({object.region, object.offset});
+}
+
+void FamObject::deallocate() {
+    impl_->deallocate();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// DATA
+
+void FamObject::put(const void* data, const fam::size_t offset, const fam::size_t length) const {
+    impl_->put(data, offset, length);
+}
+
+void FamObject::get(void* buffer, const fam::size_t offset, const fam::size_t length) const {
+    impl_->get(buffer, offset, length);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// ATOMIC
+
+template<typename T>
+auto FamObject::fetch(const fam::size_t offset) const -> T {
+    return impl_->fetch<T>(offset);
+}
+
+template<typename T>
+void FamObject::set(const fam::size_t offset, const T value) const {
+    impl_->set<T>(offset, value);
+}
+
+template<typename T>
+void FamObject::add(const fam::size_t offset, const T value) const {
+    impl_->add<T>(offset, value);
+}
+
+template<typename T>
+auto FamObject::swap(const fam::size_t offset, const T value) const -> T {  // NOLINT
+    return impl_->swap<T>(offset, value);
+}
+
+template<typename T>
+auto FamObject::compareSwap(const fam::size_t offset, const T oldValue, const T newValue) const -> T {
+    return impl_->compareSwap<T>(offset, oldValue, newValue);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+auto FamObject::regionId() const -> uint64_t {
+    return impl_->object()->get_global_descriptor().regionId;
+}
+
+auto FamObject::offset() const -> uint64_t {
+    return impl_->object()->get_global_descriptor().offset;
+}
+
+auto FamObject::size() const -> fam::size_t {
+    return impl_->size();
+}
+
+auto FamObject::permissions() const -> fam::perm_t {
+    return impl_->permissions();
+}
+
+auto FamObject::name() const -> std::string {
+    return impl_->name();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void FamObject::print(std::ostream& out) const {
+    out << "FamObject[" << *impl_ << "]";
+}
+
+std::ostream& operator<<(std::ostream& out, const FamObject& object) {
+    object.print(out);
+    return out;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// forward instantiations
+
+template void FamObject::set(const fam::size_t, const int32_t) const;
+template void FamObject::set(const fam::size_t, const int64_t) const;
+template void FamObject::set(const fam::size_t, const openfam::int128_t) const;
+template void FamObject::set(const fam::size_t, const uint32_t) const;
+template void FamObject::set(const fam::size_t, const uint64_t) const;
+template void FamObject::set(const fam::size_t, const float) const;
+template void FamObject::set(const fam::size_t, const double) const;
+
+template auto FamObject::fetch(const fam::size_t) const -> int32_t;
+template auto FamObject::fetch(const fam::size_t) const -> int64_t;
+template auto FamObject::fetch(const fam::size_t) const -> openfam::int128_t;
+template auto FamObject::fetch(const fam::size_t) const -> uint32_t;
+template auto FamObject::fetch(const fam::size_t) const -> uint64_t;
+template auto FamObject::fetch(const fam::size_t) const -> float;
+template auto FamObject::fetch(const fam::size_t) const -> double;
+
+template auto FamObject::swap(const fam::size_t, const int32_t) const -> int32_t;
+template auto FamObject::swap(const fam::size_t, const int64_t) const -> int64_t;
+template auto FamObject::swap(const fam::size_t, const uint32_t) const -> uint32_t;
+template auto FamObject::swap(const fam::size_t, const uint64_t) const -> uint64_t;
+template auto FamObject::swap(const fam::size_t, const float) const -> float;
+template auto FamObject::swap(const fam::size_t, const double) const -> double;
+
+template auto FamObject::compareSwap(const fam::size_t, const int32_t, const int32_t) const -> int32_t;
+template auto FamObject::compareSwap(const fam::size_t, const int64_t, const int64_t) const -> int64_t;
+template auto FamObject::compareSwap(const fam::size_t, const uint32_t, const uint32_t) const -> uint32_t;
+template auto FamObject::compareSwap(const fam::size_t, const uint64_t, const uint64_t) const -> uint64_t;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace eckit
---
+++ src/eckit/io/fam/FamObject.h
@@ -0,0 +1,115 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*
+ * This software was developed as part of the Horizon Europe programme funded project OpenCUBE
+ * (Grant agreement: 101092984) horizon-opencube.eu
+ */
+
+/// @file   FamObject.h
+/// @author Metin Cakircali
+/// @date   Mar 2024
+
+#pragma once
+
+#include "eckit/io/fam/FamProperty.h"
+
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace eckit {
+
+class FamObjectDetail;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class FamObject {
+public:  // types
+    using UPtr = std::unique_ptr<FamObject>;
+
+    // using Detail = std::unique_ptr<FamObjectDetail>;
+
+    // public:  // factory methods
+    //     static auto proxy(const FamConfig& config, const Descriptor& object) -> UPtr;
+
+public:  // methods
+    explicit FamObject(std::unique_ptr<FamObjectDetail> object) noexcept;
+
+    ~FamObject();
+
+    // auto proxy(const Descriptor& object) -> UPtr;
+
+    bool operator==(const FamObject& other) const;
+
+    bool operator!=(const FamObject& other) const { return !operator==(other); }
+
+    void replace(const FamDescriptor& object);
+
+    void deallocate();
+
+    auto regionId() const -> std::uint64_t;
+
+    auto offset() const -> std::uint64_t;
+
+    auto descriptor() const -> FamDescriptor { return {regionId(), offset()}; }
+
+    auto size() const -> fam::size_t;
+
+    auto permissions() const -> fam::perm_t;
+
+    auto name() const -> std::string;
+
+    auto property() const -> FamProperty { return {size(), permissions(), name()}; }
+
+    void put(const void* data, fam::size_t offset, fam::size_t length) const;
+
+    void get(void* buffer, fam::size_t offset, fam::size_t length) const;
+
+    template<typename T>
+    auto get(const fam::size_t offset) const -> T {
+        auto buffer = T {0};
+        get(&buffer, offset, sizeof(T));
+        return buffer;
+    }
+
+    template<typename T>
+    void put(const T& data, const fam::size_t offset) const {
+        put(&data, offset, sizeof(T));
+    }
+
+    template<typename T>
+    void set(fam::size_t offset, T value) const;
+
+    template<typename T>
+    auto fetch(fam::size_t offset) const -> T;
+
+    template<typename T>
+    void add(fam::size_t offset, T value) const;
+
+    template<typename T>
+    auto swap(fam::size_t offset, T value) const -> T;
+
+    template<typename T>
+    auto compareSwap(fam::size_t offset, T oldValue, T newValue) const -> T;
+
+private:  // methods
+    void print(std::ostream& out) const;
+
+    friend std::ostream& operator<<(std::ostream& out, const FamObject& object);
+
+private:  // members
+    std::unique_ptr<FamObjectDetail> impl_;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace eckit

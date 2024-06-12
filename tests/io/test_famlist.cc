303498e4743badb018da10f621cafbf966a8d48b
Metin Cakircali <metin.cakircali@ecmwf.int>
2024-06-12 16:27:51 +0200

    fix(FAM): FamList tests

--- tests/io/test_famlist.cc
+++ tests/io/test_famlist.cc
@@ -31,60 +31,66 @@
 using namespace eckit;
 using namespace eckit::testing;
 
+namespace eckit::test {
+
 //----------------------------------------------------------------------------------------------------------------------
 // HELPERS
 
 namespace {
 
-constexpr const fam::size_t numThreads = 8;
-constexpr const fam::size_t listSize   = 200;
-constexpr const auto        listName   = "ECKIT_TEST_FAM_CATALOGUE";
+fam::TestFam tester;
+
+constexpr const auto numThreads = 8;
+constexpr const auto listSize   = 20;
+const auto           listName   = "ECKIT_TEST_FAM_LIST_" + test::fam::randomNumber();
 
 std::vector<std::string> testData;
 std::mutex               testMutex;
 
-auto makeTestData(const int number) -> std::string {
+auto makeTestData(const int number) -> std::string_view {
     std::ostringstream oss;
-    oss << "[tid:" << std::this_thread::get_id() << "] ECKIT TEST DATA #" << number;
-    return oss.str();
+    oss << "[tid:" << std::this_thread::get_id() << "] ECKIT FAM TEST DATA #" << number;
+    // add to the control list
+    const std::lock_guard<std::mutex> lock(testMutex);
+    return testData.emplace_back(oss.str());
 }
 
-void createList() {
-    FamList lst(test::fam::region(), listName);
-
+void populateList() {
+    FamList lst(*tester.getLastRegion(), listName);
     for (auto i = 0; i < listSize; i++) {
-        const auto buffer = makeTestData(i);
-        // insert the buffer into the FAM list
+        auto buffer = makeTestData(i);
         EXPECT_NO_THROW(lst.push_back(buffer.data(), buffer.size()));
-        // insert the buffer into the control list
-        const std::lock_guard<std::mutex> lock(testMutex);
-        testData.emplace_back(buffer);
     }
 }
 
 }  // namespace
 
-namespace eckit::test {
-
 //----------------------------------------------------------------------------------------------------------------------
 
-CASE("FamList: empty list") {
-    const auto lst = FamList(fam::region(), listName);
+CASE("FamList: create an empty list and validate size, empty, front, back") {
+    auto listRegion = tester.makeRandomRegion(1024);
+
+    const auto lst = FamList(*listRegion, listName);
 
     EXPECT(lst.empty());
 
     EXPECT(lst.size() == 0);
+
+    EXPECT(lst.front().size() == 0);
+    EXPECT(lst.front().data() == nullptr);
+
+    EXPECT(lst.back().size() == 0);
+    EXPECT(lst.back().data() == nullptr);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-CASE("FamList: create a list and insert " + std::to_string(listSize) + " items concurrently by " +
-     std::to_string(numThreads) + " threads") {
+CASE("FamList: populate with " + std::to_string(listSize) + " items by " + std::to_string(numThreads) + " threads") {
     std::vector<std::thread> threads;
 
     threads.reserve(numThreads);
 
-    for (auto i = 0; i < numThreads; i++) { threads.emplace_back(createList); }
+    for (auto i = 0; i < numThreads; i++) { threads.emplace_back(populateList); }
 
     for (auto&& thread : threads) { thread.join(); }
 }
@@ -92,7 +98,7 @@ CASE("FamList: create a list and insert " + std::to_string(listSize) + " items c
 //----------------------------------------------------------------------------------------------------------------------
 
 CASE("FamList: validate size and values after creation") {
-    const auto lst = FamList(fam::region(), listName);
+    const auto lst = FamList(*tester.getLastRegion(), listName);
 
     EXPECT_NOT(lst.empty());
 
@@ -108,10 +114,5 @@ CASE("FamList: validate size and values after creation") {
 //----------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char** argv) {
-    auto ret = run_tests(argc, argv);
-
-    // cleanup
-    test::fam::region()->destroy();
-
-    return ret;
+    return run_tests(argc, argv);
 }

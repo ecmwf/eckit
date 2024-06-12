be4cf01dcec040823550de11e295fa477f853756
Metin Cakircali <metin.cakircali@ecmwf.int>
2024-06-12 21:58:06 +0200

    test(FAM): added more region and object API stuff

--- tests/io/test_fam.cc
+++ tests/io/test_fam.cc
@@ -27,32 +27,113 @@ using namespace eckit::testing;
 
 namespace eckit::test {
 
-using namespace fam;
+// using namespace fam;
+
+// namespace {
+//
+// // fam::TestFam tester;
+// FamName name {testEndpoint};
+//
+// }  // namespace
 
 //----------------------------------------------------------------------------------------------------------------------
 
-CASE("FamRegion: lookup, create, and destroy") {
-    // cleanup
-    destroyRegions({regionName});
+CASE("FamRegion: lookup, create, validate properties, and destroy") {
+    const auto regionName = fam::TestFam::makeRandomText("REGION");
+    const auto regionSize = 1024;
+    const auto regionPerm = static_cast<eckit::fam::perm_t>(0640);
 
-    const FamProperty property {1024, 0640, regionName};
+    FamName name {fam::testEndpoint + '/' + regionName};
 
-    // not found
-    EXPECT_THROWS_AS(FamRegion::lookup(property.name, config), NotFound);
+    EXPECT_THROWS_AS(name.lookupRegion(), NotFound);
+
+    EXPECT_NO_THROW(name.createRegion(regionSize, regionPerm));
 
     FamRegion::UPtr region;
 
-    EXPECT_NO_THROW(region = FamRegion::create(property, config));
+    EXPECT_NO_THROW(region = name.lookupRegion().clone());
 
-    EXPECT(region->property() == property);
+    EXPECT(region->size() == regionSize);
 
-    EXPECT_NO_THROW(region = FamRegion::lookup(regionName, config));
+    EXPECT(region->permissions() == regionPerm);
 
-    EXPECT(region->property() == property);
+    EXPECT(region->name() == regionName);
+
+    {
+        const FamProperty prop {regionSize, regionPerm, regionName};
+        EXPECT(region->property() == prop);
+    }
 
     EXPECT_NO_THROW(region->destroy());
 
-    EXPECT_THROWS_AS(FamRegion::lookup(property.name, config), NotFound);
+    EXPECT_THROWS_AS(name.lookupRegion(), NotFound);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+CASE("FamObject: lookup, create, and destroy") {
+    const auto regionName = fam::TestFam::makeRandomText("REGION");
+    const auto regionSize = 64;
+    const auto regionPerm = static_cast<eckit::fam::perm_t>(0640);
+
+    const auto objectName = fam::TestFam::makeRandomText("OBJECT");
+    const auto objectSize = 24;
+    const auto objectPerm = static_cast<eckit::fam::perm_t>(0400);
+
+    // FamName API
+    {
+        const auto uri = fam::testEndpoint + '/' + regionName + '/' + objectName;
+
+        // ctor string URI - region and object
+        const auto name = FamName(uri);
+
+        EXPECT_THROWS_AS(name.lookupRegion(), NotFound);
+        EXPECT_THROWS_AS(name.lookupObject(), NotFound);
+
+        name.createRegion(regionSize, regionPerm);
+
+        FamObject::UPtr object;
+
+        // object inherits permissions from region
+        EXPECT_NO_THROW(object = name.allocateObject(objectSize).clone());
+
+        const FamProperty prop {objectSize, regionPerm, objectName};
+        EXPECT(object->property() == prop);
+
+        EXPECT_NO_THROW(object->deallocate());
+    }
+
+    // FamRegion API
+    {
+        // ctor endpoint only
+        auto name = FamName(fam::testEndpoint);
+
+        auto region = name.with(regionName).lookupRegion();
+
+        EXPECT_THROWS_AS(region.lookupObject(objectName), NotFound);
+
+        {
+            const auto size = 12;
+            EXPECT_NO_THROW(region.allocateObject(size, objectPerm, objectName));
+            EXPECT(region.lookupObject(objectName).size() == size);
+        }
+
+        // overwrite: allocate with different size
+        EXPECT_NO_THROW(region.allocateObject(objectSize, objectPerm, objectName, true));
+
+        auto object = region.lookupObject(objectName);
+
+        const FamProperty prop {objectSize, objectPerm, objectName};
+        EXPECT(object.property() == prop);
+
+        EXPECT_NO_THROW(object.deallocate());
+
+        EXPECT_THROWS_AS(region.lookupObject(objectName), NotFound);
+
+        EXPECT_NO_THROW(region.destroy());
+
+        EXPECT_THROWS_AS(name.lookupRegion(), NotFound);
+    }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -60,10 +141,5 @@ CASE("FamRegion: lookup, create, and destroy") {
 }  // namespace eckit::test
 
 int main(int argc, char** argv) {
-    auto ret = run_tests(argc, argv);
-
-    // cleanup
-    test::fam::destroyRegions({test::regionName});
-
-    return ret;
+    return run_tests(argc, argv);
 }

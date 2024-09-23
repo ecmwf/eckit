79c660ff41c8715b4cd8c0f0469f3ddad7c1d7c2
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2024-09-23 15:24:05 +0100

    eckit::geo FESOM, ORCA

---
+++ src/eckit/geo/Arrangement.cc
@@ -0,0 +1,41 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/geo/Arrangement.h"
+
+#include "eckit/exception/Exceptions.h"
+
+
+namespace eckit::geo {
+
+
+Arrangement arrangement_from_string(const std::string& str) {
+    return str == "F"   ? Arrangement::F
+           : str == "T" ? Arrangement::T
+           : str == "U" ? Arrangement::U
+           : str == "V" ? Arrangement::V
+           : str == "W" ? Arrangement::W
+                        : throw AssertionFailed("Arrangement", Here());
+}
+
+
+std::string arrangement_to_string(Arrangement a) {
+    return a == Arrangement::F   ? "F"
+           : a == Arrangement::T ? "T"
+           : a == Arrangement::U ? "U"
+           : a == Arrangement::V ? "V"
+           : a == Arrangement::W ? "W"
+                                 : throw AssertionFailed("Arrangement", Here());
+}
+
+
+}  // namespace eckit::geo

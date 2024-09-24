7c1dbe65dc36d1c2a9faf78ab128f48bde1ac33f
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2024-09-24 21:37:42 +0100

    eckit::geo FESOM, ORCA

--- src/eckit/geo/grid/FESOM.h
+++ src/eckit/geo/grid/FESOM.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * (C) Copyright 1996- ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
@@ -13,10 +13,14 @@
 #pragma once
 
 #include <cstddef>
-#include <vector>
 
 #include "eckit/geo/Arrangement.h"
 #include "eckit/geo/grid/Unstructured.h"
+
+
+namespace eckit {
+class PathName;
+}
 
 
 namespace eckit::geo::grid {
@@ -24,24 +28,45 @@ namespace eckit::geo::grid {
 
 class FESOM final : public Unstructured {
 public:
+    // -- Types
+
+    struct FESOMRecord {
+        explicit FESOMRecord() = default;
+
+        void read(const PathName&);
+
+        using bytes_t = decltype(sizeof(int));
+        bytes_t footprint() const;
+
+        size_t n() const;
+
+        std::vector<double> longitudes_;
+        std::vector<double> latitudes_;
+    };
+
     // -- Constructors
 
     explicit FESOM(const Spec&);
     explicit FESOM(uid_t);
 
+    // -- Methods
+
+    std::string arrangement() const;
+
     // -- Overridden methods
 
     uid_t calculate_uid() const override;
+
+    // -- Class methods
+
+    [[nodiscard]] static Spec* spec(const std::string& name);
 
 private:
     // -- Members
 
     uid_t uid_;
     Arrangement arrangement_;
-    std::string path_;
-
-    std::vector<double> longitudes_;
-    std::vector<double> latitudes_;
+    const FESOMRecord& record_;
 
     // -- Overridden methods
 

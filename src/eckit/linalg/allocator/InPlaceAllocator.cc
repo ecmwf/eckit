a2fb7dd4374a66d0f841a3209175efbdb9e264d9
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2024-11-13 12:56:36 +0000

    eckit::linalg::SparseMatrix::Allocator new InPlaceAllocator

---
+++ src/eckit/linalg/allocator/InPlaceAllocator.cc
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2024- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/linalg/allocator/InPlaceAllocator.h"
+
+#include <ostream>
+
+#include "eckit/exception/Exceptions.h"
+
+
+namespace eckit::linalg::allocator {
+
+
+InPlaceAllocator::InPlaceAllocator(Size Nr, Size Nc, Size nnz, Index* ia, Index* ja, Scalar* a) :
+    Nr_(Nr), Nc_(Nc), nnz_(nnz), ia_(ia), ja_(ja), a_(a) {
+    ASSERT(ia_ != nullptr);
+    ASSERT(ja_ != nullptr);
+    ASSERT(a_ != nullptr);
+}
+
+
+SparseMatrix::Layout InPlaceAllocator::InPlaceAllocator::allocate(SparseMatrix::Shape& shape) {
+    shape.size_ = nnz_;
+    shape.rows_ = Nr_;
+    shape.cols_ = Nc_;
+
+    SparseMatrix::Layout layout;
+    layout.outer_ = reinterpret_cast<decltype(SparseMatrix::Layout::outer_)>(ia_);
+    layout.inner_ = ja_;
+    layout.data_  = a_;
+
+    return layout;
+}
+
+
+void InPlaceAllocator::deallocate(SparseMatrix::Layout, SparseMatrix::Shape) {}
+
+
+bool InPlaceAllocator::inSharedMemory() const {
+    return false;
+}
+
+
+void InPlaceAllocator::print(std::ostream& out) const {
+    out << "InPlaceAllocator[Nr=" << Nr_ << ",Nc=" << Nc_ << ",nnz=" << nnz_ << "]";
+}
+
+
+}  // namespace eckit::linalg::allocator

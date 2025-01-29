66d13fd8b9cb1894b109ea665615350caf0b8784
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-01-29 15:41:18 +0000

    Headers

--- src/mir/search/PointSearch.cc
+++ src/mir/search/PointSearch.cc
@@ -19,6 +19,7 @@
 #include "mir/param/MIRParametrisation.h"
 #include "mir/repres/Iterator.h"
 #include "mir/repres/Representation.h"
+#include "mir/util/Exceptions.h"
 #include "mir/util/Log.h"
 #include "mir/util/Trace.h"
 #include "mir/util/Types.h"

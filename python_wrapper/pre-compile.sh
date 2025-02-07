b5af57c5e04154c84b9718121b90daa9135e8169
Vojta Tuma <vojta.tuma@gmail.com>
2025-02-07 11:14:55 +0100

    Python wheel: add curl dependency

---
+++ python_wrapper/pre-compile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# the procedure for adding a new ext dependency to be bundled in here:
+# - add git checkout, compile, etc
+# - ensure the version ends up in python_wrapper/src/versions.txt
+# - ensure the licence ends up in python_wrapper/src/copying/, and fname is referenced in copying/list.json
+# - ensure the .so ends up in target/lib64/ with the expected libname
+# - validate that the resulting wheel contains all the above
+# additionally, make sure this script is aligned with /buildscripts/compile.sh and /buildscripts/wheel-linux.sh,
+# in particular when it comes to install targets and package data, etc
+
+# note also that for macos, we assume that the agent has the libraries already installed, as we can't run this in docker
+# we thus only prepare the license files
+
+set -euo pipefail
+
+mkdir -p python_wrapper/src/copying
+mkdir -p /tmp/eckit/target/eckit/lib64/
+
+if [ "$(uname)" != "Darwin" ] ; then
+    echo "installing deps for platform $(uname)"
+
+    ## yum-available prereqs
+    yum install -y libpsl-devel openssl-devel 
+
+    ## curl
+    git clone https://github.com/curl/curl /src/curl
+    cd /src/curl
+    autoreconf -fi
+    ./configure --prefix /tmp/curl --with-openssl
+    make -j10 && make install
+    cd -
+
+    # NOTE we will use auditwheel in post-compile. But in case of problems like we had with eccodes, introduce the libcopies here
+else
+    # TODO check macos agent has curl
+    echo "no deps installation for platform $(uname)"
+fi
+
+wget https://raw.githubusercontent.com/rockdaboot/libpsl/master/LICENSE -O python_wrapper/src/copying/libpsl.txt
+wget https://raw.githubusercontent.com/openssl/openssl/master/LICENSE.txt -O python_wrapper/src/copying/libssl.txt
+wget https://raw.githubusercontent.com/gnosis/libunistring/master/COPYING -O python_wrapper/src/copying/libunistring.txt
+wget https://raw.githubusercontent.com/libidn/libidn2/master/COPYINGv2 -O python_wrapper/src/copying/libidn2.txt
+cp /src/curl/COPYING python_wrapper/src/copying/libcurl.COPYING
+cp -R /src/curl/LICENSES python_wrapper/src/copying/libcurl
+echo '{"libpsl": {"path": "copying/libpsl.txt", "home": "https://github.com/rockdaboot/libpsl"}, "libssl": {"path": "copying/libssl.txt", "home": "https://github.com/openssl/openssl"}, "libcrypto": {"path": "copying/libssl.txt", "home": "https://github.com/openssl/openssl"}, "libunistring": {"path": "copying/libunistring.txt", "home": "https://github.com/gnosis/libunistring/"}, "libidn2": {"path": "copying/libidn2.txt", "home": "https://github.com/libidn/libidn2"}, "libcurl": {"path": "copying/liburl.COPYING", "home": "https://github.com/curl/curl"}}' > python_wrapper/src/copying/list.json

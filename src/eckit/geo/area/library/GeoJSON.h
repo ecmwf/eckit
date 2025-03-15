d1354dff50a86c864c5bc343a9cac38f7cbeb30c
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-03-15 14:48:03 +0000

    eckit::geo GeoJSON

--- src/eckit/geo/area/library/GeoJSON.h
+++ src/eckit/geo/area/library/GeoJSON.h
@@ -17,13 +17,18 @@
 #include <vector>
 
 #include "eckit/geo/polygon/LonLatPolygon.h"
+#include "eckit/value/Value.h"
 
 
-namespace mir {
-namespace geography {
+namespace eckit {
+class PathName;
+}
 
 
-class GeographyInput {
+namespace eckit::geo::area::library {
+
+
+class GeoJSON {
 public:
     // -- Types
 
@@ -36,183 +41,22 @@ public:
     using Polygon  = std::unique_ptr<eckit::geo::polygon::LonLatPolygon>;
     using Polygons = std::vector<Polygon>;
 
-    enum Geometry
-    {
-        LonLat,
-        Spherical
-    };
-
-    // -- Exceptions
-    // None
-
-    // -- Constructors
-    // None
-
-    // -- Destructor
-    // None
-
-    // -- Convertors
-    // None
-
-    // -- Operators
-    // None
-
-    // -- Methods
-
-    virtual std::vector<Polygons> polygons() const = 0;
-    static std::string geometryAsString(Geometry);
-    static Geometry stringAsGeometry(const std::string&);
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-protected:
-    // -- Members
-
-    explicit GeographyInput(Geometry);
-
-    // -- Methods
-
-    Geometry geometry() const { return geometry_; }
-    std::string geometryAsString() const;
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-private:
-    // -- Members
-
-    const Geometry geometry_;
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Friends
-    // None
-};
-
-
-}  // namespace geography
-}  // namespace mir
-/*
- * (C) Copyright 1996- ECMWF.
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation nor
- * does it submit to any jurisdiction.
- */
-
-
-#pragma once
-
-#include "eckit/parser/JSONParser.h"
-
-
-namespace eckit {
-class PathName;
-}
-
-
-namespace mir {
-namespace geography {
-
-
-class GeoJSON : public GeographyInput {
-public:
-    // -- Types
-    // None
-
-    // -- Exceptions
-    // None
-
     // -- Constructors
 
-    explicit GeoJSON(const eckit::PathName&, Geometry);
-    explicit GeoJSON(std::string& json, Geometry);
-
-    // -- Destructor
-    // None
-
-    // -- Convertors
-    // None
-
-    // -- Operators
-    // None
+    explicit GeoJSON(const eckit::PathName&);
+    explicit GeoJSON(std::string& json);
 
     // -- Methods
 
-    std::vector<Polygons> polygons() const override;
+    std::ostream& list(std::ostream&) const;
 
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-protected:
-    // -- Members
-    // None
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
+    std::vector<Polygons> polygons() const;
 
 private:
     // -- Members
 
     eckit::Value json_;
-
-    // -- Methods
-    // None
-
-    // -- Overridden methods
-    // None
-
-    // -- Class members
-    // None
-
-    // -- Class methods
-    // None
-
-    // -- Friends
-    // None
 };
 
 
-}  // namespace geography
-}  // namespace mir
+}  // namespace eckit::geo::area::library

e08896b08cb6c3e1677568334063d2552da0ff7f
Vojta Tuma <vojta.tuma@gmail.com>
2025-04-02 14:13:58 +0200

    Better auditwheel

--- python_wrapper/post-build.sh
+++ python_wrapper/post-build.sh
@@ -3,8 +3,15 @@
 # NOTE in case of problems like we had with eccodes, replace with noop here
 if [ "$(uname)" != "Darwin" ] ; then
     auditwheel repair -w /tmp/eckit/auditwheel /tmp/eckit/build/wheel/*whl
+    cd /tmp/eckit/auditwheel
+    F=$(ls $whl)
+    unzip $F
+    patchelf --add-rpath '$ORIGIN' eckitlib.libs/*
+    rm $F
+    zip -r $F ./*
     rm /tmp/eckit/build/wheel/*
-    mv /tmp/eckit/auditwheel/* /tmp/eckit/build/wheel
+    mv /tmp/eckit/auditwheel/$F /tmp/eckit/build/wheel
+    cd -
 fi
 
 # NOTE on macos we delocate with impunity, because the findlibs recursive depload

ded3a7a82d3362b1ae91b84c669f7d4faf392ffb
Vojta Tuma <vojta.tuma@gmail.com>
2025-04-10 13:13:55 +0200

    [python] reorg directories

--- python/eckitlib/pre-compile.sh
+++ python/eckitlib/pre-compile.sh
@@ -14,7 +14,7 @@
 
 set -euo pipefail
 
-mkdir -p python_wrapper/src/copying
+mkdir -p python/eckitlib/src/copying
 mkdir -p /tmp/eckit/target/eckit/lib64/
 
 if [ "$(uname)" != "Darwin" ] ; then
@@ -29,5 +29,5 @@ else
 fi
 
 
-wget https://raw.githubusercontent.com/lz4/lz4/dev/LICENSE -O python_wrapper/src/copying/liblz4.txt
-echo '{"liblz4": {"path": "copying/liblz4.txt", "home": "https://github.com/lz4/lz4"}}' > python_wrapper/src/copying/list.json
+wget https://raw.githubusercontent.com/lz4/lz4/dev/LICENSE -O python/eckitlib/src/copying/liblz4.txt
+echo '{"liblz4": {"path": "copying/liblz4.txt", "home": "https://github.com/lz4/lz4"}}' > python/eckitlib/src/copying/list.json

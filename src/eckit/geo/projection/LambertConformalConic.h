95e9a6504bff33e937741e4ffe0b40075b1986a9
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-05-16 10:36:26 +0100

    eckit::geo projection

--- src/eckit/geo/projection/LambertConformalConic.h
+++ src/eckit/geo/projection/LambertConformalConic.h
@@ -30,9 +30,8 @@ public:
 
     explicit LambertConformalConic(const Spec&);
 
-    LambertConformalConic(PointLonLat centre, PointLonLat first, double lat_1, double lat_2);
-    LambertConformalConic(PointLonLat centre, PointLonLat first, double lat_1) :
-        LambertConformalConic(centre, first, lat_1, lat_1) {}
+    explicit LambertConformalConic(double lat_1, double lon_0 = 0., double lat_0 = 0., double lat_2 = 0.,
+                                   Figure* = nullptr);
 
     // -- Methods
 
@@ -56,16 +55,17 @@ private:
 
     // -- Members
 
-    const PointLonLat centre_;     // central meridian/parallel [degree]
-    const PointLonLatR centre_r_;  // central meridian/parallel [radian]
+    /// Central meridian/longitude of natural origin, longitude of origin or longitude of false origin
+    const double lon_0_;
 
-    const PointLonLat first_;     // first point [degree]
-    const PointLonLatR first_r_;  // first point [radian]
+    /// Latitude of natural origin, latitude of false origin or latitude of projection centre
+    const double lat_0_;
 
+    /// First standard parallel
     const double lat_1_;
-    const double lat_1_r_;
+
+    /// Second standard parallel
     const double lat_2_;
-    const double lat_2_r_;
 
     double n_;
     double f_;

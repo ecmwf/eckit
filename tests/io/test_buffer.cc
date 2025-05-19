e65b284598f3202754ccf5d455af5612a55923d9
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2025-05-19 15:41:16 +0100

    format

--- tests/io/test_buffer.cc
+++ tests/io/test_buffer.cc
@@ -28,9 +28,9 @@ unsigned char* random_bytestream(const size_t sz) {
     return stream;
 }
 
-constexpr auto msg    = "Once upon a midnight dreary";
+constexpr auto msg = "Once upon a midnight dreary";
 // std::char_traits<T>::length _should_ be constexpr. But this doesn't work with the old GCC used in the DHS
-constexpr auto msgLen = 27; //std::char_traits<char>::length(msg);
+constexpr auto msgLen = 27;  // std::char_traits<char>::length(msg);
 
 CASE("Test eckit Buffer default constructor") {
     Buffer buf;

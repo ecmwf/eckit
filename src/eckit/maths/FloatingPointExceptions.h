d05b27546eadd88e5427bce0958c1531a45006f3
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-06-13 16:38:11 +0100

    eckit::Fraction floating point exception handling

--- src/eckit/maths/FloatingPointExceptions.h
+++ src/eckit/maths/FloatingPointExceptions.h
@@ -1,87 +1,34 @@
 /*
- * (C) Copyright 2013 ECMWF.
+ * (C) Copyright 2013- ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  * In applying this licence, ECMWF does not waive the privileges and immunities
- * granted to it by virtue of its status as an intergovernmental organisation
- * nor does it submit to any jurisdiction.
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
  */
 
-#include <csignal>
-#include <iosfwd>
-#include <map>
 #include <string>
-#include <cfenv>
 
-namespace atlas {
-namespace library {
+namespace eckit::maths {
 
 // ------------------------------------------------------------------------------------
 
-/* @brief Enable floating point exceptions
- * 
- * An environment variable is responsible to enable:
- * - ATLAS_FPE=0                  --> Default. do not enable anything in Atlas (other libraries or programs may still do so)
- * - ATLAS_FPE=1                  --> FE_INVALID, FE_DIVBYZERO, FE_OVERFLOW
- * - ATLAS_FPE=VALUE1,VALUE2,...  --> Specified codes only. Valid codes: 
- *                                    FE_INVALID, FE_DIVBYZERO, FE_OVERFLOW, FE_UNDERFLOW, FE_INEXACT, FE_ALL_EXCEPT
- * 
- * @note This function is called automatically within Library::initialize() and should
- *       not be called directly
- */
-void enable_floating_point_exceptions();
+class FloatingPointExceptions {
+public:
 
-/* @brief Enable floating point exception
- * 
- * Valid codes: 
- *   FE_INVALID, FE_DIVBYZERO, FE_OVERFLOW, FE_UNDERFLOW, FE_INEXACT, FE_ALL_EXCEPT
- * 
- * @return false when the exception was already enabled, true when change was made
- */
-bool enable_floating_point_exception(const std::string&);
-bool enable_floating_point_exception(int);
+    /// Enable floating point exceptions
+    static void enable_floating_point_exceptions(const std::string& = "FE_DIVBYZERO, FE_INVALID, FE_OVERFLOW");
 
-/* @brief Disable floating point exception
- * 
- * Valid codes: 
- *   FE_INVALID, FE_DIVBYZERO, FE_OVERFLOW, FE_UNDERFLOW, FE_INEXACT, FE_ALL_EXCEPT
- * 
- * @return false when the exception was already disabled, true when change was made
- */
-bool disable_floating_point_exception(const std::string&);
-bool disable_floating_point_exception(int);
+    /// Disable floating point exceptions
+    static void disable_floating_point_exceptions(const std::string& = "");
+
+    /// Enable atlas signal handler for SIGFPE, SIGILL
+    static void enable_signal_handler();
+
+    [[noreturn]] static void custom_signal_handler(int signum, ::siginfo_t* si, void* unused);
+};
 
 // ------------------------------------------------------------------------------------
 
-/* @brief Enable atlas signal handler for all signals
- * 
- * An environment variable is responsible to enable:
- * - ATLAS_SIGNAL_HANDLER=0   --> Default. Atlas will not set any signal handlers
- * - ATLAS_SIGNAL_HANDLER=1   --> Enable atlas_signal_handler instead of the default for signals:
- * 
- * Enabled signals:
- * - SIGABRT
- * - SIGFPE
- * - SIGILL
- * - SIGINT
- * - SIGSEGV
- * - SIGTERM
- * - SIGKILL
- * 
- * @note This function is called automatically within Library::initialize() and should
- *       not be called directly
- */
-void enable_atlas_signal_handler();
-
-// ------------------------------------------------------------------------------------
-
-using signal_handler_t = void (*)(int);
-using signal_action_t  = void (*)(int, ::siginfo_t*, void*);
-
-[[noreturn]] void atlas_signal_handler(int signum, ::siginfo_t* si, void* unused);
-
-// ------------------------------------------------------------------------------------
-
-}  // namespace library
-}  // namespace atlas
+}  // namespace eckit::maths

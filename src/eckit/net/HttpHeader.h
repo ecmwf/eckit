c9688c47b08b2c20c3438415d4ba5871fdfd3d52
Simon Smart <simon.smart@ecmwf.int>
2026-03-06 19:54:43 +0000

    Add HTTP return code 413 (Content too large)

--- src/eckit/net/HttpHeader.h
+++ src/eckit/net/HttpHeader.h
@@ -41,6 +41,7 @@ public:
         SEE_OTHER             = 303,
         BAD_REQUEST           = 400,
         UNAUTHORIZED          = 401,
+        CONTENT_TOO_LARGE     = 413,
         NOT_FOUND             = 404,
         NOT_IMPLEMENTED       = 501,
         INTERNAL_SERVER_ERROR = 500,

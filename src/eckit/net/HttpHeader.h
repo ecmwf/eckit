98f8959e50034facefa4c27e76cb3719f389e098
Simon Smart <simon.smart@ecmwf.int>
2026-03-06 20:02:24 +0000

    Fix typo

--- src/eckit/net/HttpHeader.h
+++ src/eckit/net/HttpHeader.h
@@ -41,8 +41,8 @@ public:
         SEE_OTHER             = 303,
         BAD_REQUEST           = 400,
         UNAUTHORIZED          = 401,
-        CONTENT_TOO_LARGE     = 413,
         NOT_FOUND             = 404,
+        CONTENT_TOO_LARGE     = 413,
         NOT_IMPLEMENTED       = 501,
         INTERNAL_SERVER_ERROR = 500,
     };

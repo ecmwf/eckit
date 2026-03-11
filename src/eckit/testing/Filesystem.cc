defefb7ec3d87b95bfa20b53b39114d02ebb3284
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2026-03-11 17:06:30 +0000

    back-porting eckit::testing::Filesystem to eckit 1.32.5

---
+++ src/eckit/testing/Filesystem.cc
@@ -0,0 +1,35 @@
+/*
+ * (C) Copyright 2025- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+#include "eckit/testing/Filesystem.h"
+
+#include <vector>
+
+namespace eckit::testing {
+
+void deldir(eckit::PathName& p) {
+    if (!p.exists()) {
+        return;
+    }
+
+    std::vector<eckit::PathName> files;
+    std::vector<eckit::PathName> dirs;
+    p.children(files, dirs);
+
+    for (auto& f : files) {
+        f.unlink();
+    }
+    for (auto& d : dirs) {
+        deldir(d);
+    }
+
+    p.rmdir();
+};
+
+}  // namespace eckit::testing
